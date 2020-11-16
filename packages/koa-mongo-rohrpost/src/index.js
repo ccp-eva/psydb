bd80bcce79815291add3b3c03e4d4d88fbc535f6
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-11-16 08:00:00 +0100

    initial version of koa-mongo-rohrpost

---
+++ packages/koa-mongo-rohrpost/src/index.js
@@ -0,0 +1,47 @@
+'use strict';
+var MongoRohrpost = require('@mpieva/mongo-rohrpost');
+
+var createMongoRohrpostMiddleware = ({
+    createChannelId,
+    createChannelEventId,
+
+    disableChannelLocking,
+}) => {
+
+    if (typeof createChannelId !== 'function') {
+        throw new Error('parameter "createChannelId" is required and must be a function');
+    }
+
+    if (typeof createChannelEventId !== 'function') {
+        throw new Error('parameter "createChannelEventId" is required and must be a function');
+    }
+    
+    return async (context, next) => {
+        var { db, correlationId } = context;
+
+        if (!db) {
+            throw new Error('failed to get database handle from context');
+        }
+        if (!correlationId) {
+            throw new Error('failed to get correlation id from context');
+        }
+
+        var rohrpost = MongoRohrpost({
+            db,
+            correlationId,
+
+            createChannelId,
+            createChannelEventId,
+            disableChannelLocking,
+        });
+
+        context.rohrpost = rohrpost;
+        await next();
+
+        if (!disableChannelLocking) {
+            await rohrpost.unlockModifiedChannels();
+        }
+    }
+};
+
+module.exports = createMongoRohrpostMiddleware;
