ad44bca58a4acb7fe699393398bdfb388367abd3
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-11-26 13:32:21 +0100

    added packages to psydb-schema-fields; email list now has "isPrimary" attribute

--- packages/psydb-schema-fields/src/email-list.js
+++ packages/psydb-schema-fields/src/email-list.js
@@ -1,11 +1,33 @@
 'use strict';
+var inline = require('@cdxoo/inline-text');
+
 var EmailList = ({ minItems }) => ({
     $schema: 'http://json-schema.org/draft-07/schema#',
     type: 'array',
     minItems: (minItems || 0),
+    // unqiueItemProperties requires "ajv-keywords"
+    uniqueItemProperties: [ 'email' ],
     items: {
-        type: 'string',
-        format: 'email'
+        type: 'object',
+        properties: {
+            email: {
+                type: 'string',
+                format: 'email'
+            },
+            isPrimary: {
+                type: 'boolean',
+                default: false,
+                description: inline`
+                    controls if this email is the primary
+                    one this is relevant when the system
+                    wants to send emails or checks login
+                `,
+            }
+        },
+        required: [
+            'email',
+            'isPrimary',
+        ]
     },
 })
 
