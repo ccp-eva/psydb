3ae098cc620838604bdb3fa67e6717a25b23386e
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-12-07 08:43:10 +0100

    added some tests to sign-in endpoint

--- packages/psydb-server/src/public-endpoints/sign-in.js
+++ packages/psydb-server/src/public-endpoints/sign-in.js
@@ -1,16 +1,19 @@
 'use strict';
+var debug = require('debug')('psydb:api:sign-in'),
+    bcrypt = require('bcrypt');
+
 var signIn = async (context, next) => {
     var { db, session, request } = context;
     var { email, password } = request.body;
 
-    var records = await (
+    var personnelRecords = await (
         db.collection('personnel').find({
             $and: [
                 { 'scientific.state.systemRoleId': {
                     $exists: true
                 }},
                 { 'scientific.state.systemRoleId': {
-                    $type: 7, // bson type of object id
+                    $not: { $type: 10 } // bson type of null
                 }},
             ],
             'gdpr.state.emails': { $elemMatch: {
@@ -18,29 +21,55 @@ var signIn = async (context, next) => {
                 isPrimary: true,
             }},
         }, {
+            'scientific.state.researchGroupIds': true,
             'gdpr.state.internals.passwordHash': true
         })
         .toArray()
     );
 
-    if (records.length < 1) {
+    if (personnelRecords.length < 1) {
+        debug('no personnel records found');
         throw new Error(); // TODO: 401
     }
-    if (records.length > 1) {
+    if (personnelRecords.length > 1) {
+        debug('multiple personnel records found');
         throw new Error(); // TODO: 401
     }
 
-    var storedHash = (
-        records[0]
-        .gdpr.state.internals.passwordHash
-    );
+    var user = personnelRecords[0],
+        storedHash = user.gdpr.state.internals.passwordHash;
+    var { researchGroupIds, systemRoleId } = user.scientific.state;
+
+    if (!systemRoleId) {
+        debug('user has no system role');
+        throw new Error(); // TODO: 401
+    }
+
+    // if the user has no research groups check if their role has
+    // root access
+    if (researchGroupIds.length < 1) {
+        var role = await (
+            db.collection('systemRole')
+            .findOne({
+                _id: systemRoleId
+            })
+        );
+        if (!(role && role.state.hasRootAccess)) {
+            debug('user has no researchgroups and role has no root acccess');
+            throw new Error(); // TODO: 401
+        }
+    }
 
     if (bcrypt.compareSync(password, storedHash)) {
-        session.userId = records[0]._id;
+        debug('passwords match, setting session personnelId');
+        session.personnelId = user._id;
     }
     else {
+        debug('passwords dont match');
         throw new Error(); // TODO: 401
     }
 
     await next();
 }
+
+module.exports = signIn;
---
+++ packages/psydb-server/src/public-endpoints/sign-in.spec.js
@@ -0,0 +1,143 @@
+'use strict';
+require('debug').enable('psydb:*');
+
+var expect = require('chai').expect,
+    Mongod = require('mongodb-memory-server').MongoMemoryServer,
+    MongoClient = require('mongodb').MongoClient,
+    bcrypt = require('bcrypt'),
+
+    signIn = require('./sign-in');
+
+describe('signIn()', function () {
+    this.timeout(0);
+    
+    var server, uri, con, db, createContext;
+    beforeEach(async () => {
+        server = new Mongod();
+        await server.start();
+        ({ uri } = server.getInstanceInfo());
+
+        con = await MongoClient.connect(
+            uri,
+            { useUnifiedTopology: true }
+        );
+
+        db = con.db('testDB');
+        
+        await initCollections(db);
+        
+        createContext = ({ email, password }) => ({
+            db,
+            session: {},
+            request: { body: {
+                email,
+                password
+            }}
+        });
+    });
+
+    afterEach(async () => {
+        if (con) {
+            con.close();
+        }
+        await server.stop();
+    });
+
+    // TODO: 401 failure cases
+
+    it('handles root role login', async () => {
+        var calledNext = false,
+            next = async () => { calledNext = true; };
+
+        var context = createContext({
+            email: 'alice@example.com',
+            password: 'alice-rocks'
+        });
+
+        await signIn(context, next);
+
+        expect(context.session.personnelId).to.eql('alice');
+        expect(calledNext).to.eql(true);
+    });
+
+    it('handles other role login', async () => {
+        var calledNext = false,
+            next = async () => { calledNext = true; };
+
+        var context = createContext({
+            email: 'bob@example.com',
+            password: 'bob-rocks'
+        });
+
+        await signIn(context, next);
+
+        expect(context.session.personnelId).to.eql('bob');
+        expect(calledNext).to.eql(true);
+    });
+
+
+
+});
+
+var initCollections = async (db) => {
+    await db.collection('systemRole').insertMany([
+        createSystemRoleRecord({
+            _id: 'root', hasRootAccess: true,
+        }),
+        createSystemRoleRecord({
+            _id: 'scientist', hasRootAccess: false,
+        }),
+    ]);
+    await db.collection('personnel').insertMany([
+        createPersonnelRecord({
+            _id: 'alice',
+            email: 'alice@example.com',
+            password: 'alice-rocks',
+            systemRoleId: 'root',
+            researchGroupIds: [],
+        }),
+        createPersonnelRecord({
+            _id: 'bob',
+            email: 'bob@example.com',
+            password: 'bob-rocks',
+            systemRoleId: 'scientist',
+            researchGroupIds: [ 'child-studies' ],
+        }),
+    ]);
+}
+
+var createSystemRoleRecord = ({
+    _id,
+    hasRootAccess,
+}) => ({
+    _id,
+    state: {
+        hasRootAccess,
+    }
+})
+
+var createPersonnelRecord = ({
+    _id,
+    email,
+    password,
+    systemRoleId,
+    researchGroupIds
+}) => ({
+    _id,
+    scientific: {
+        state: {
+            systemRoleId,
+            researchGroupIds: researchGroupIds || [],
+        }
+    },
+    gdpr: {
+        state: {
+            emails: [
+                { email, isPrimary: true }
+            ],
+            internals: {
+                passwordHash: bcrypt.hashSync(password, 10)
+            }
+        }
+    }
+});
