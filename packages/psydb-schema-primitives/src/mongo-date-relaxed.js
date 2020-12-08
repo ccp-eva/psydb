28b735a2e9d9a29a3b2e3af7ba6cc01e4d874227
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-12-08 11:59:09 +0100

    added fixme, that i removed elsehwere

--- packages/psydb-schema-primitives/src/mongo-date-relaxed.js
+++ packages/psydb-schema-primitives/src/mongo-date-relaxed.js
@@ -1,6 +1,26 @@
 'use strict';
+
+// FIXME:
+// we need to make sure we emulate the upcoming v3.0 behavior
+// mongo extjson in regards to datetime i.e.: always use iso string
+// see:
+// https://github.com/mongodb/specifications/blob/master/source/extended-json.rst#datetime
+// extjson.stringify(
+//     { foo: new Date('1600-01-01') },
+//     (a, o) => { 
+//         if (a === '$date') {
+//              return (new Date(parseInt(o['$numberLong']))).toISOString()
+//         }
+//         else {
+//             return o; 
+//         }
+//     },
+//     { relaxed: true }
+// )
+
 var MongoDateRelaxed = ({
-    formatMinimum
+    formatMinimum,
+    additionalKeywords,
 } = {}) => ({
     type: "object",
     properties: {
@@ -16,7 +36,8 @@ var MongoDateRelaxed = ({
     },
     required: [
         "$date"
-    ]
+    ],
+    ...additionalKeywords,
 })
 
 module.exports = MongoDateRelaxed
