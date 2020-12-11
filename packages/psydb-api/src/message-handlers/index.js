b3235265fb15995ae4a844e4d6bb0232d0e35cd0
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-12-11 17:04:33 +0100

    (psydb-api) added minimal handler mapping

---
+++ packages/psydb-api/src/message-handlers/index.js
@@ -0,0 +1,34 @@
+'use strict';
+var handlers = [
+    {
+        messageType: /^records\//,
+        handler: require('./records')
+    },
+    /*{
+        messageType: 'set-personnel-password',
+        handler: require('./set-personnel-password'),
+    }*/
+];
+
+handlers.find = (messageType) => {
+    var filtered = handlers.filter(it => {
+        if (it.messageType instanceof RegExp) {
+            return it.messageType.test(messageType)
+        }
+        else {
+            return it.messageType === messageType
+        }
+    });
+
+    if (filtered.length < 1) {
+        throw new Error(`no message handler "${messageType}"`);
+    }
+    else if (filtered.length > 1) {
+        throw new Error(`multiple message handlers "${messageType}"`);
+    }
+    else {
+        return filtered[0].handler;
+    }
+}
+
+module.exports = handlers;
