c7c48a3ff522aba4d0a34f5b750ba448274eff10
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-03-03 05:33:13 +0100

    classroom demo type and fixes to reservation settings

--- packages/psydb-schema/src/collection/location/reservation-settings-schema.js
+++ packages/psydb-schema/src/collection/location/reservation-settings-schema.js
@@ -2,40 +2,61 @@
 var inline = require('@cdxoo/inline-text');
 
 var {
+    ExactObject,
     ForeignId,
     DateTimeInterval,
     TimeInterval,
 } = require('@mpieva/psydb-schema-fields');
 
 var reservationSettingsSchema = {
-    type: 'object',
-    properties: {
-        canBeReservedByResearchGroup: {
-            type: 'array',
-            default: [],
-            items: ForeignId('researchGroup'),
-        },
-        disabledForReservationIntervals: {
-            type: 'array',
-            default: [],
-            items: DateTimeInterval(),
-        },
+    oneOf: [
+        ExactObject({
+            properties: {
+                canBeReserved: {
+                    type: 'boolean',
+                    enum: [ false ],
+                    default: false, // because rjsf
+                },
+            },
+            required: [ 'canBeReserved' ]
+        }),
+        ExactObject({
+            properties: {
+                canBeReserved: {
+                    type: 'boolean',
+                    enum: [ true ],
+                    default: true, // because rjsf
+                },
+                canBeReservedByResearchGroupIds: {
+                    type: 'array',
+                    default: [],
+                    items: ForeignId('researchGroup'),
+                },
+                disabledForReservationIntervals: {
+                    type: 'array',
+                    default: [],
+                    items: DateTimeInterval(),
+                },
 
-        possibleReservationTimeInterval: TimeInterval(),
+                possibleReservationTimeInterval: TimeInterval(),
 
-        reservationSlotDuration: {
-            type: 'integer',
-            minimum: 15*60*1000, // 15 minutes
-            maximum: 4*60*60*1000, // 4 hours (arbitrary)
-            multipleOf: 15*60*1000, // 15 minutes
-        },
-    },
-    required: [
-        'canBeReservedByResearchGroupIds',
-        'disabledForReservationIntervals',
-        'possibleReservationTimeInterval',
-        'reservationSlotDuration',
-    ],
+                reservationSlotDuration: {
+                    type: 'integer',
+                    minimum: 15*60*1000, // 15 minutes
+                    maximum: 4*60*60*1000, // 4 hours (arbitrary)
+                    multipleOf: 15*60*1000, // 15 minutes
+                },
+            },
+            required: [
+                'canBeReserved',
+                'canBeReservedByResearchGroupIds',
+                'disabledForReservationIntervals',
+                'possibleReservationTimeInterval',
+                'reservationSlotDuration',
+            ],
+        })
+    ]
+
 };
 
 module.exports = reservationSettingsSchema;
