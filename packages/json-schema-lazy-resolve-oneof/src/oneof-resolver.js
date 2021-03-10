1fb2add8010a78c4623496e8023d7fa9445df484
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-03-10 17:40:04 +0100

    added missing resolver file (json-schema-lazy-resolve-oneof)

---
+++ packages/json-schema-lazy-resolve-oneof/src/oneof-resolver.js
@@ -0,0 +1,102 @@
+'use strict';
+var OneofResolver = () => {
+    var resolver = {},
+        transformations = [];
+
+    resolver.transformations = () => (transformations);
+
+    resolver.resolve = ({
+        currentData,
+        traverseArgs,
+    }) => {
+        var [
+            currentSchema,
+            inSchemaPointer,
+            rootSchema,
+            parentInSchemaPointer,
+            parentKeyword,
+            parentSchema,
+            propNameOrIndex
+        ] = traverseArgs;
+
+        if (!currentSchema.oneOf) {
+            throw new Error(inline`
+                oneOf keyword not found in "${inSchemaPointer}"
+            `);
+        }
+
+        var lazyResolveProp = currentSchema.lazyResolveProp;
+        if (!lazyResolveProp) {
+            throw new Error(inline`
+                no "lazyResolveProp" definition in "${inSchemaPointer}"
+            `)
+        }
+
+        var branches = currentSchema.oneOf,
+            branchWasFound = false;
+        for (var [index, branchSchema] of branches.entries()) {
+            var lazyResolveDataValue = currentData[lazyResolveProp];
+            var lazyResolveSchema = (
+                branchSchema.properties[lazyResolveProp]
+            );
+            
+            var shouldUseBranch = decide({
+                schema: lazyResolveSchema,
+                dataValue: lazyResolveDataValue
+            });
+
+            if (shouldUseBranch && branchWasFound) {
+                throw new Error(inline`
+                    multiple matching branches found in path
+                    "${inSchemaPointer}"
+                `);
+            }
+
+            if (shouldUseBranch) {
+                branchWasFound = true;
+                transformations.push({
+                    from: inSchemaPointer,
+                    to: `${inSchemaPointer}/oneOf/${index}`
+                })
+            }
+        }
+
+        if (!branchWasFound) {
+            throw new Error(inline`
+                no valid branch was found in ${inSchemaPointer}
+            `);
+        }
+
+    }
+
+    return resolver;
+};
+
+var decide = ({
+    schema,
+    dataValue
+}) => {
+    var shouldUse = false;
+
+    if (schema.enum) {
+        var schemaValue = schema.enum;
+        if (schemaValue.includes(dataValue)) {
+            shouldUse = true;
+        }
+    }
+    else if (schema.const) {
+        var schemaValue = schema.const;
+        if (schemaValue === dataValue) {
+            shouldUse = true;
+        }
+    }
+    else {
+        throw new Error(inline`
+            oneof resolver can only handle properties via "enum" or "const"
+        `);
+    }
+
+    return shouldUse;
+}
+
+module.exports = OneofResolver;
