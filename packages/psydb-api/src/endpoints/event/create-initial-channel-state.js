16824a053165f0032694f5843a2ca797755448d9
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-03-15 08:38:10 +0100

    initial channel state creation now uses psydb-schema-creators (psydb-api)

--- packages/psydb-api/src/endpoints/event/create-initial-channel-state.js
+++ packages/psydb-api/src/endpoints/event/create-initial-channel-state.js
@@ -1,12 +1,14 @@
 'use strict';
 var Ajv = require('../../lib/ajv');
 
-var {
+/*var {
     collectionMetadata,
     CustomRecordTypeState,
     HelperSetState,
     HelperSetItemState,
-} = require('@mpieva/psydb-schema');
+} = require('@mpieva/psydb-schema');*/
+
+var allSchemaCreators = require('@mpieva/psydb-schema-creators');
 
 var createInitialChannelState = async ({
     collectionName: collection,
@@ -26,7 +28,77 @@ var createInitialChannelState = async ({
         disableProhibitedKeyword: true
     });
 
-    var channelStateSchema = undefined;
+    var collectionCreatorData = allSchemaCreators[collection];
+    if (!collectionCreatorData) {
+        throw new Error(`no creator data found for collection "${collection}"`);
+    }
+    
+    var {
+        isGenericRecord,
+        hasCustomTypes,
+        hasSubChannels,
+        subChannelStateSchemaCreators,
+    } = collectionCreatorData;
+
+    if (subChannelKey) {
+        if (!hasSubChannels) {
+            throw new Error(inline`
+                collection "${collection}" does not support subchannels
+            `);
+        }
+        if (!subChannelStateSchemaCreators[subChannelKey]) {
+            throw new Error(inline`
+                collection "${collection}" has no schema creator for
+                sub-channel key "${subChannelKey}"
+            `);
+        }
+    }
+    else {
+        if (hasSubChannels) {
+            throw new Error(inline`
+                collection "${collection}" has sub channels but no key
+                was providede
+            `);
+        }
+    }
+
+    var args = {
+        enableInternalProps: true,
+    };
+
+    if (hasCustomTypes) {
+        var customRecordType = await findCustomRecordType({
+            db,
+            collection,
+            type: stored.type
+        });
+    
+        if (hasSubChannels) {
+            args.subChannelCustomRecordFieldDefinitions = (
+                customRecordType.state.settings.subChannelFields
+            );
+        }
+        else {
+            args.customFieldDefinitions = (
+                customRecordType.state.settings.fields
+            );
+        }
+    }
+
+    // this collection needs extra argument
+    if (collection === 'customRecordType') {
+        args.collection = stored.collection;
+    }
+
+    var StateCreator = (
+        hasSubChannels
+        ? subChannelStateSchemaCreators[subChannelKey]
+        : collectionCreatorData.State
+    );
+
+    var channelStateSchema = StateCreator({ ...args });
+
+    /*var channelStateSchema = undefined;
     if (collection === 'helperSet') {
         channelStateSchema = HelperSetState({
             enableInternalProps: true
@@ -43,11 +115,6 @@ var createInitialChannelState = async ({
             enableInternalProps: true
         });
     }
-    /*else if (collection === 'teamSchedule') {
-        channelStateSchema = ScheduleState({
-
-        })
-    }*/
     else {
         var { type, subtype } = stored;
         
@@ -69,7 +136,7 @@ var createInitialChannelState = async ({
             ? recordSchemas[subChannelKey]
             : recordSchemas.state
         );
-    }
+    }*/
 
     // although called validate() it will initialize
     // the default values of required properties 
@@ -81,6 +148,41 @@ var createInitialChannelState = async ({
     return {
         state: initialState,
     }
+}
+
+var findCustomRecordType = async ({ db, collection, type }) => {
+    var customRecordTypes = await (
+        db.collection('customRecordType').find(
+            { collection, type },
+            { 'state.isNew': true, 'state.settings': true }
+        ).toArray()
+    );
+
+    if (customRecordTypes.length < 1) {
+        throw new Error(inline`
+            no customRecordType entry found for
+            collection "${collection}" with type "${type}"
+        `);
+    }
+    else if (customRecordTypes.length > 1) {
+        throw new Error(inline`
+            multiple customRecordType entries found for
+            collection "${collection}" with type "${type}"
+        `);
+    }
+
+    console.log(customRecordTypes);
+    var customRecordType = customRecordTypes[0];
+    if (customRecordType.isNew) {
+        throw new Error(inline`
+            custom record type for collection "${collection}"
+            with type "${type}" is flagged as "new" and
+            has never been commited; please create an 
+            initial commit first
+        `);
+    }
+    
+    return customRecordType;
 }
 
 module.exports = createInitialChannelState;
