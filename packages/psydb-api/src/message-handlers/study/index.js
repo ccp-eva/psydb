9eaa514226bd0f4a371d4265bf73394de3ee3c2d
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-03-23 14:04:45 +0100

    added stub study message handler group

---
+++ packages/psydb-api/src/message-handlers/study/index.js
@@ -0,0 +1,39 @@
+'use strict';
+var {
+    MessageHandlerGroup
+} = require('@mpieva/psydb-koa-event-middleware');
+
+// TODO
+var StudyGroup = MessageHandlerGroup([
+    // creates new record and add core attributes
+    // such as name etc (all custom record props)
+    require('./create-core'),
+    // patch core attributes of the study
+    require('./patch-core'),
+
+    // add a subject type that may hold conditions and can be
+    // tested in this study
+    require('./add-subject-type'),
+    // removes subject type 
+    require('./remove-subject-type'),
+
+    // adds a condition that must be met by testable subjects
+    // used when searching for testable subjects
+    require('./add-search-condition'),
+    // updates existing search condition for example
+    // when required values are changed
+    require('./update-search-condition'),
+    // remove an existing search condition
+    require('./remove-search-condition'),
+
+    // commits changes to subject types and conditions
+    require('./commit-types-and-conditions'),
+    // resets uncommited changes to types and conditions
+    require('./reset-types-and-conditions'),
+
+    // deletes study record that has never been commited
+    // and is flagged as "isNew"
+    require('./cancel-create'),
+]);
+
+module.exports = StudyGroup;
