9402abb5090147cc203ea01aac2f838aaa19b14e
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-04-09 12:56:29 +0200

    added production webpack config

---
+++ packages/psydb-ui/src/webpack.config-prod.js
@@ -0,0 +1,38 @@
+'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
+    context: __dirname,
+    mode: 'production',
+    entry: './index.js',
+    devtool: false,
+    output: {
+        //path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '',
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './template.html'),
+            title: 'psydb',
+        }),
+        new CompressionPlugin(),
+    ]
+};
