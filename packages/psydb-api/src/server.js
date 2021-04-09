b78193833f4d36568a81ea56442bda786c4679ba
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-04-09 11:10:12 +0200

    added missing packages; updates api-server start script, added script to call init endpoint

--- packages/psydb-api/src/server.js
+++ packages/psydb-api/src/server.js
@@ -1,17 +1,16 @@
 var Koa = require('koa'),
-    compose = require('koa-compose'),
-    maybeSession = require('koa-session'),
-    maybeConnectMongoDB = require('db').middleware;
+    createApi = require('./middleware/api');
 
-var app = new Koa(),
-    composition = createKoaComposition(app, {
+var app = new Koa();
+
+app.use(
+    createApi(app, {
         db: {
             url: 'mongodb://127.0.0.1:27017/psydb',
             dbName: 'psydb',
             useUnifiedTopology: true,
         }
-    });
-
-app.use(composition);
+    })
+);
 
 app.listen(3012);
