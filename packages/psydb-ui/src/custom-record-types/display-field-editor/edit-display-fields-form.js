78a0a74d61de12df02fb7ee98c98b4db3f53c8c8
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-04-12 10:34:13 +0200

    extracted field pointer list control

--- packages/psydb-ui/src/custom-record-types/display-field-editor/edit-display-fields-form.js
+++ packages/psydb-ui/src/custom-record-types/display-field-editor/edit-display-fields-form.js
@@ -2,33 +2,7 @@ import React, { useState, useReducer } from 'react';
 import { Button } from 'react-bootstrap';
 import agent from '@mpieva/psydb-ui-request-agents';
 
-import DisplayFieldSelector from './display-field-selector';
-import DisplayFieldList from './display-field-list';
-
-const moveInsitu = (ary, fromIndex, toIndex) => {
-    if (toIndex >= ary.length) {
-        var k = toIndex - ary.length + 1;
-        while (k--) {
-            ary.push(undefined);
-        }
-    }
-    ary.splice(toIndex, 0, ary.splice(fromIndex, 1)[0]);
-};
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'move':
-            var { from, to } = action.payload;
-            var clone = [ ...state ];
-            moveInsitu(clone, from, to);
-            return clone;
-        case 'add':
-            var { item } = action.payload;
-            return [ ...state, item ];
-        default:
-            throw new Error('unkown action type')
-    }
-}
+import FieldPointerListControl from './field-pointer-list-control';
 
 const EditDisplayFieldForm = ({
     target,
@@ -37,12 +11,8 @@ const EditDisplayFieldForm = ({
     availableDisplayFieldDataByDataPointer,
     onSuccess,
 }) => {
-    target = target || 'table';
 
-    var [ state, dispatch ] = useReducer(
-        reducer,
-        currentDataPointers
-    );
+    var [ state, setState ] = useState(currentDataPointers);
 
     var handleSaveChanges = () => {
         var messageBody = {
@@ -68,37 +38,15 @@ const EditDisplayFieldForm = ({
         ) 
     }
 
-    var handleMoveItem = ({ from, to }) => {
-        console.log(from, to);
-        dispatch({
-            type: 'move',
-            payload: { from, to }
-        });
+    var handleChange = (nextValue) => {
+        setState(nextValue);
     }
-
-    var handleAddItem = (dataPointer) => {
-        console.log(dataPointer);
-        dispatch({
-            type: 'add',
-            payload: { item: dataPointer }
-        })
-    }
-
-    console.log(state);
 
     return (
         <>
-        <DisplayFieldList
-            onMoveItem={ handleMoveItem }
-            dataPointers={ state }
-            availableDisplayFieldDataByDataPointer={
-                availableDisplayFieldDataByDataPointer
-            }
-        />
-        <hr />
-        <DisplayFieldSelector
-            onSelect={ handleAddItem }
-            selectedDataPointers={ state }
+        <FieldPointerListControl
+            value={ state }
+            onChange={ handleChange }
             availableDisplayFieldDataByDataPointer={
                 availableDisplayFieldDataByDataPointer
             }
---
+++ packages/psydb-ui/src/custom-record-types/display-field-editor/field-pointer-list-control.js
@@ -0,0 +1,57 @@
+import React, { useState, useReducer } from 'react';
+import { Button } from 'react-bootstrap';
+import agent from '@mpieva/psydb-ui-request-agents';
+
+import DisplayFieldSelector from './display-field-selector';
+import DisplayFieldList from './display-field-list';
+
+const moveInsitu = (ary, fromIndex, toIndex) => {
+    if (toIndex >= ary.length) {
+        var k = toIndex - ary.length + 1;
+        while (k--) {
+            ary.push(undefined);
+        }
+    }
+    ary.splice(toIndex, 0, ary.splice(fromIndex, 1)[0]);
+};
+
+const FieldPointerListControl = ({
+    value,
+    onChange,
+    availableDisplayFieldDataByDataPointer,
+}) => {
+
+    var handleMoveItem = ({ from, to }) => {
+        console.log(from, to);
+        var clone = [ ...value ];
+        moveInsitu(clone, from, to);
+        onChange(clone);
+    }
+
+    var handleAddItem = (dataPointer) => {
+        console.log(dataPointer);
+        onChange([ ...value, dataPointer ]);
+    }
+
+    return (
+        <>
+        <DisplayFieldList
+            onMoveItem={ handleMoveItem }
+            dataPointers={ value }
+            availableDisplayFieldDataByDataPointer={
+                availableDisplayFieldDataByDataPointer
+            }
+        />
+        <hr />
+        <DisplayFieldSelector
+            onSelect={ handleAddItem }
+            selectedDataPointers={ value }
+            availableDisplayFieldDataByDataPointer={
+                availableDisplayFieldDataByDataPointer
+            }
+        />
+        </>
+    )
+}
+
+export default FieldPointerListControl;
