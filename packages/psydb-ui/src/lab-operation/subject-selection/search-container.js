7133c3a98b4161fa46335c68e74dcac55e21b16c
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-04-23 02:57:29 +0200

    more search container related code

--- packages/psydb-ui/src/lab-operation/subject-selection/search-container.js
+++ packages/psydb-ui/src/lab-operation/subject-selection/search-container.js
@@ -11,11 +11,92 @@ import {
 
 import agent from '@mpieva/psydb-ui-request-agents';
 
+var reducer = (state, action) => {
+    var { type, payload } = state;
+    switch (type) {
+        case 'change-experiment-time-interval':
+            return ({
+                ...state,
+                experimentTimeInterval: payload
+            });
+        case 'init-ageframe-settings':
+            return ({
+                ...state,
+                // TODO: descie how to structure that
+                ageFrameSettingsByStudy: payload
+            })
+    }
+};
+
 const SearchContainer = () => {
     var { path, url } = useRouteMatch();
+
+    useEffect(() => {
+        agent.fetchAgeFrameSettings({
+            studyIds,
+            subjectRecordType,
+        })
+        .then(response => {
+            dispatch({
+                type: 'init-ageframe-settings',
+                payload: response.data.data.ageFrameSettingsByStudy
+                // [studyId]: [
+                //    { ageFrame: {}, conditionList }
+                // ]
+            })
+        })
+    })
+
     return (
-        <div>FOOOO</div>
+        <Switch>
+            <Route exact path={ `${path}` }>
+                <ExperimentTimeIntervalEditor
+                    interval={ experimentTimeInterval }
+                    onChange={ (nextInterval) => {
+                        dispatch({
+                            type: 'change-experiment-time-interval',
+                            payload: nextInterval
+                        })
+                    }}
+                />
+                <AgeFrameSettingsEditor
+                    settings={ settings }
+                    onChange={ (...args) => {
+                        console.log(args);
+                    }}
+                />
+            </Route>
+            <Route exact path={ `${path}/search`}>
+                <TestableSubjectList />
+            </Route>
+        </Switch>
     )
+}
+
+const ExperimentTimeIntervalEditor = ({
+    interval,
+    onChange,
+}) => {
+    return (
+        <div>Interval editor</div>
+    )
+}
+
+const AgeFrameSettingsEditor = ({
+    settings,
+    onChange
+}) => {
+    return (
+        <div>Age frame ditor</div>
+    )
+}
+
+const TestableSubjectList = ({
+    settings
+}) => {
+    return (
+        <div>testable subject list</div>
+    );
 }
 
 export default SearchContainer;
