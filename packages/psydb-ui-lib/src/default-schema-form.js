f04aea616f360f6c8c5a54f3f8aa01d8eb12e874
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-05-27 22:42:09 +0200

    added validation error indication next to the form button in case of long form

--- packages/psydb-ui-lib/src/default-schema-form.js
+++ packages/psydb-ui-lib/src/default-schema-form.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useState }from 'react';
 
 import { Button } from 'react-bootstrap';
 
@@ -11,16 +11,29 @@ const DefaultSchemaForm = ({
     buttonLabel,
     ...downstream
 }) => {
+    var [ hasValidationError, setValidationError ] = useState(undefined);
+    var handleValidationError = (errors) => {
+        setValidationError(errors);
+    }
+
     return (
         <SchemaForm
             noHtml5Validate={ true }
             showErrorList={ false }
+            onError={ handleValidationError }
             { ...downstream }
         >
-            <div>
+            <div className='d-flex align-items-center'>
                 <Button type="submit" className="btn btn-primary">
                     { buttonLabel || 'Speichern' }
                 </Button>
+                <div className='pl-3 pr-3'>
+                    { hasValidationError && (
+                        <b className='text-danger'>
+                            Ungültige Eingabedaten!
+                        </b>
+                    )}
+                </div>
             </div>
         </SchemaForm>
     );
