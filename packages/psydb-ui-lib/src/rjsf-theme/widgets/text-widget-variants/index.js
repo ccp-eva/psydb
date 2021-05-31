70e27c1a36ab9c8fd85c0cc74b11a36f041b719c
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-05-31 07:31:09 +0200

    added rjsf time widget variant

--- packages/psydb-ui-lib/src/rjsf-theme/widgets/text-widget-variants/index.js
+++ packages/psydb-ui-lib/src/rjsf-theme/widgets/text-widget-variants/index.js
@@ -2,3 +2,4 @@ export { default as PlainText } from './plain-text';
 export { default as FullText } from './full-text';
 export { default as ForeignId } from './foreign-id';
 export { default as HelperSetItemId } from './helper-set-item-id';
+export { default as Time } from './time';
