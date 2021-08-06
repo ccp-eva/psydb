ecb2d1de3167f3d5faf003e2bde3b742f7a49784
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-08-06 13:54:54 +0200

    updated indication when timeslot had an experiment

--- packages/psydb-ui-lib/src/study-inhouse-locations/location-reservation-calendar/time-slot.js
+++ packages/psydb-ui-lib/src/study-inhouse-locations/location-reservation-calendar/time-slot.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useReducer, useMemo } from 'react';
+import { PersonFill } from 'react-bootstrap-icons';
+
 import datefns from '../../date-fns';
 import getTextColor from '../../bw-text-color-for-background';
 
@@ -42,10 +44,10 @@ const TimeSlot = ({
                     height: '26px',
                     background: teamRecord.state.color,
                     color: getTextColor(teamRecord.state.color),
-                    borderWidth: '2px',
-                    borderStyle: 'dashed',
-                    borderColor: getTextColor(teamRecord.state.color),
-                    boxSizing: 'border-box'
+                    //borderWidth: '2px',
+                    //borderStyle: 'dashed',
+                    //borderColor: getTextColor(teamRecord.state.color),
+                    //boxSizing: 'border-box'
                 }}
                 onClick={ () => {
                     onSelectExperimentSlot && onSelectExperimentSlot({
@@ -58,13 +60,31 @@ const TimeSlot = ({
                     })
                 }}
             >
-                <b className='d-inline-block pl-1 pr-1' style={{
-                    //borderLeft: '4px solid',
-                    //borderRight: '4px solid',
-                    //borderColor: getTextColor(teamRecord.state.color),
-                }}>
-                    <u>{ datefns.format(date, 'p') }</u>
-                </b>
+                <div className='d-flex'>
+                    <b className='d-inline-block pl-1 pr-1 flex-grow' style={{
+                        height: '26px',
+                        //borderLeft: '4px solid',
+                        //borderRight: '4px solid',
+                        //borderColor: getTextColor(teamRecord.state.color),
+                    }}>
+                        <span>{ datefns.format(date, 'p') }</span>
+                    </b>
+                    <span
+                        className={
+                            'bg-white text-center'
+                        }
+                        style={{
+                            color: '#333',
+
+                            border: '1px solid #333',
+                            
+                            height: '26px',
+                            width: '30px'
+                        }}
+                    >
+                        <PersonFill style={{ width: '20px', height: '20px', marginTop: '-3px' }} />
+                    </span>
+                </div>
             </div>
         );
     }
