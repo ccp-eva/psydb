d1755944067b4530d3eb3820b81fac050ce1725b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-10-01 04:21:09 +0200

    removed old code

--- packages/psydb-ui/src/lab-operation/subject-selection/inhouse/experiment-create-modal.js
+++ packages/psydb-ui/src/lab-operation/subject-selection/inhouse/experiment-create-modal.js
@@ -1,94 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import { useSend } from '@mpieva/psydb-ui-hooks';
 
+import { useSend } from '@mpieva/psydb-ui-hooks';
 import ExperimentShortControls from '@mpieva/psydb-ui-lib/src/experiment-short-controls';
 
-import { withTheme } from '@rjsf/core';
-import { Theme as Bootstrap4Theme } from '@rjsf/bootstrap-4'
-
-import agent from '@mpieva/psydb-ui-request-agents';
 import datefns from '@mpieva/psydb-ui-lib/src/date-fns';
-
-import {
-    Duration,
-    FormattedDuration
-} from '@mpieva/psydb-common-lib/src/durations';
-
-import {
-    ConstWidget,
-    TimeSlotWidget,
-    ExperimentOperatorTeamIdWidget,
-} from '@mpieva/psydb-ui-lib/src/rjsf-components';
-
-const extractTime = (dateIsoString) => (
-    NaN !== (new Date(dateIsoString)).getTime()
-    ? datefns.format(new Date(dateIsoString), 'HH:mm:ss.SSS') + 'Z'
-    : dateIsoString
-);
-
-var createSchema = ({
-    subjectLabel,
-
-    start,
-    slotDuration,
-    maxEnd
-}) => ({
-    type: 'object',
-    properties: {
-        subject: {
-            title: 'Proband',
-            type: 'string',
-            const: subjectLabel,
-            default: subjectLabel,
-        },
-        date: {
-            title: 'Datum',
-            type: 'string',
-            const: datefns.format(start, 'PPPP'),
-            default: datefns.format(start, 'PPPP')
-        },
-        start: {
-            title: 'Von',
-            type: 'string',
-            const: extractTime(start).slice(0,5),
-            default: extractTime(start).slice(0,5)
-        },
-        end: {
-            title: 'Bis',
-            type: 'string',
-            format: 'time',
-            default: extractTime(
-                new Date(start.getTime() + (slotDuration || 0))
-            ),
-            formatMinimum: extractTime(
-                new Date(start.getTime() + (slotDuration || 0))
-            ),
-            formatMaximum: extractTime(maxEnd),
-            formatStep: FormattedDuration(slotDuration) + 'Z',
-        },
-    },
-    required: [
-        'end',
-    ]
-})
-
-var uiSchema = {
-    'subject': {
-        'ui:widget': ConstWidget,
-    },
-    'date': {
-        'ui:widget': ConstWidget,
-    },
-    'start': {
-        'ui:widget': ConstWidget,
-    },
-    'end': {
-        'ui:widget': TimeSlotWidget
-    },
-}
-
-var SchemaForm = withTheme(Bootstrap4Theme);
 
 const CreateModal = ({
     show,
@@ -114,20 +30,6 @@ const CreateModal = ({
     var experimentOperatorTeamId = (
         reservationRecord.state.experimentOperatorTeamId
     );
-    //var fallbackDate = new Date(0);
-    
-    /*start = start || fallbackDate;
-    maxEnd = maxEnd || fallbackDate;
-    console.log(slotDuration);
-    slotDuration = slotDuration || Duration('0:30')*/
-
-    var schema = createSchema({
-        subjectLabel,
-
-        start,
-        slotDuration,
-        maxEnd,
-    });
 
     var startOfDay = datefns.startOfDay(start).getTime();
 
@@ -192,11 +94,6 @@ const CreateModal = ({
                         Speichern
                     </Button>
                 </div>
-                {/*<SchemaForm
-                    schema={ schema }
-                    uiSchema={ uiSchema }
-                    onSubmit={ handleSubmit }
-                />*/}
             </Modal.Body>
         </Modal>
     );
