026fa22414818f4d6a0ee6617c8c9ba254009886
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-11-23 05:02:19 +0100

    added stub wraper package for react-router-dom although i dint need it actually

---
+++ packages/psydb-ui-router-dom/src/index.js
@@ -0,0 +1,20 @@
+import {
+    Route,
+    Switch,
+    Redirect,
+
+    useRouteMatch,
+    useHistory,
+    useParams
+} from 'react-router-dom';
+
+export {
+    Route,
+    Switch
+    Redirect,
+
+    useRouteMatch,
+    useHistory,
+    useParams
+}
+
