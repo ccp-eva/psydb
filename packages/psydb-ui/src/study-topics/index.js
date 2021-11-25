d478c9eb0522295a0a506bd030928119078a713f
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-11-25 06:34:47 +0100

    working visuals of topic overview

--- packages/psydb-ui/src/study-topics/index.js
+++ packages/psydb-ui/src/study-topics/index.js
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import classnames from 'classnames';
 
 import {
     useFetch,
@@ -10,7 +11,8 @@ import {
 import {
     LoadingIndicator,
     Icons,
-    Button
+    Button,
+    PageWrappers
 } from '@mpieva/psydb-ui-layout';
 
 import CreateModal from './create-modal';
@@ -31,6 +33,8 @@ const StudyTopics = () => {
         createModal.handleShow({ id })
     );
 
+    var [ selectedTopicId, onSelect ] = useState();
+
     var [ didFetch, fetched ] = useFetch((agent) => (
         agent.getAxios().post('/api/study-topic-tree', {
             name: undefined,
@@ -44,36 +48,90 @@ const StudyTopics = () => {
     var { trees } = fetched.data;
 
     return (
-        <>
-            <CreateModal
-                { ...createModal.passthrough }
-                onSuccessfulUpdate={ revision.up }
-            />
+        <PageWrappers.Level1 title='Themengebiete'>
+            <PageWrappers.Level2 title='Übersicht'>
+                <CreateModal
+                    { ...createModal.passthrough }
+                    onSuccessfulUpdate={ revision.up }
+                />
 
-            { trees.map((it, index) => (
-                <Topic key={ index } { ...it } />
-            ))}
-            <Button onClick={ () => onCreate() }>
-                Neues Thema
-            </Button>
-        </>
+                <Button onClick={ () => onCreate() }>
+                    Neues Hauptthema
+                </Button>
+
+                <ActionContext.Provider value={{
+                    onCreate,
+                    onEdit,
+                    onSelect,
+                    selectedTopicId
+                }}>
+                    { trees.map((it, index) => (
+                        <Topic key={ index } { ...it } />
+                    ))}
+                </ActionContext.Provider>
+
+            </PageWrappers.Level2>
+        </PageWrappers.Level1>
     );
 }
 
 const Topic = (ps) => {
-    var { data, children, level = 0 } = ps;
-    var { _id, state } = data;
+    var {
+        onCreate,
+        onEdit,
+        onSelect,
+        selectedTopicId
+    } = useContext(ActionContext);
+
+    var { data: node, children, level = 0 } = ps;
+    var { _id, state } = node;
     var { name, parentId } = state;
 
+    //var Name = [0,1].includes(level) ? `h${4+level}` : 'div'
+    var Name = level === 0 ? 'h5' : 'div'
+    var isSelected = (selectedTopicId === _id);
+
+    var wrapperClassName = classnames([
+        level === 0 ? 'mb-2' : 'ml-4'
+    ]);
+    var nameClassName = classnames([
+        'd-inline-block mr-2 pb-1',
+        level === 0 && 'mt-2'
+        //isSelected && 'text-primary text-bold'
+    ]);
+
     return (
-        <div>
-            <div>{ name }</div>
-            { children.map((it, index) => (
-                <Topic
-                    key={ index } level={ level + 1}
-                    { ...it }
-                />
-            ))}
+        <div className={ wrapperClassName }>
+            <Name className='m-0'>
+                <span
+                    className={ nameClassName }
+                    onClick={ () => onSelect(_id) }
+                >
+                    { name }
+                </span>
+                { isSelected && (
+                    <a
+                        className='btn btn-link p-0 m-0 border-0'
+                        style={{ verticalAlign: 'baseline' }}
+                        onClick={ () => onCreate(node) }
+                    >
+                        <Icons.Plus
+                            viewBox='4 4 10 10'
+                        />
+                    </a>
+                )}
+            </Name>
+            <div
+                className='ml-1 pl-1'
+                style={{ borderLeft: '3px solid #ccc' }}
+            >
+                { children.map((it, index) => (
+                    <Topic
+                        key={ index } level={ level + 1 }
+                        { ...it }
+                    />
+                ))}
+            </div>
         </div>
     )
 }
