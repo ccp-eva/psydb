ad85c83a60454747ede8275872a7cad2378a0f53
Jan Schwalbe <jan.schwalbe@martaflex.de>
2021-12-03 07:03:51 +0100

    moved some code to local utils

--- packages/psydb-api/src/endpoints/extended-search/subjects.js
+++ packages/psydb-api/src/endpoints/extended-search/subjects.js
@@ -59,6 +59,12 @@ var RequestBodySchema = () => ExactObject({
     ]
 })
 
+var {
+    createCustomQueryValues,
+    getCustomQueryPointer,
+    convertPointerKeys
+} = require('./utils');
+
 var subjectExtendedSearch = async (context, next) => {
     var {
         db,
@@ -147,79 +153,6 @@ var subjectExtendedSearch = async (context, next) => {
     });
 }
 
-var createCustomQueryValues = (options) => {
-    var { fields, filters } = options;
-    var values = {};
-
-    for (var field of fields) {
-        var { key } = field;
-        var filter = filters[key];
-        if (filter) {
-            var pointer = getCustomQueryPointer({ field });
-            var value = createOneCustomQueryValue({
-                field, filter: filters[key]
-            });
-
-            values[pointer] = value;
-        }
-    }
-
-    return values;
-}
-
-var createOneCustomQueryValue = (options) => {
-    var { field, filter } = options;
-    var { type } = field;
-    switch (type) {
-        case 'SaneString':
-        case 'PhoneList':
-        case 'EmailList':
-            return new RegExp(filter, 'i');
-
-        case 'BiologicalGender':
-        case 'ExtBool':
-            filter = filter || {};
-            return { $in: Object.keys(filter).filter(key => !!filter[key]) }
-
-        case 'ForeignId':
-        case 'ForeignIdList':
-        case 'HelperSetItemId':
-        case 'HelperSetItemIdList':
-            filter = filter || {};
-            var op = filter.negate ? '$nin' : '$in';
-            return { [op]: filter.values };
-    }
-}
-
-var getCustomQueryPointer = (options) => {
-    var { field } = options;
-    var { type, pointer } = field;
-
-    switch (type) {
-        case 'SaneString':
-        case 'ForeignId':
-        case 'ForeignIdList':
-        case 'HelperSetItemId':
-        case 'HelperSetItemIdList':
-        case 'BiologicalGender':
-        case 'ExtBool':
-            return pointer;
-
-        case 'PhoneList':
-            return `${pointer}/number`;
-        case 'EmailList':
-            return `${pointer}/email`;
-    }
-}
-
-var convertPointerKeys = (obj) => {
-    var converted = Object.keys(obj).reduce((acc, key) => ({
-        ...acc,
-        [ convertPointerToPath(key) ]: obj[key],
-    }), {});
-
-    return converted;
-}
 
 module.exports = subjectExtendedSearch;
 
