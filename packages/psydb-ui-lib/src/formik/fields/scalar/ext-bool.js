4b266aca54c762e932dc01cdf45486d7ef8f51cd
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-01-28 08:17:02 +0100

    changed extbool to be button group

--- packages/psydb-ui-lib/src/formik/fields/scalar/ext-bool.js
+++ packages/psydb-ui-lib/src/formik/fields/scalar/ext-bool.js
@@ -1,13 +1,76 @@
 import React from 'react';
-import WithField from '../../with-field';
+import { withField } from '@cdxoo/formik-utils';
+import { ButtonGroup, Button } from '@mpieva/psydb-ui-layout';
 import { GenericEnum } from './generic-enum';
 
-const options = {
-    'yes': 'Ja',
-    'no': 'Nein',
-    'unknown': 'Unbekannt',
+const enumeration = {
+    keys: [ 'yes', 'no', 'unknown' ],
+    labels: [ 'Ja', 'Nein', 'Unbekannt' ],
 }
 
-export const ExtBool = (ps) => (
-    <GenericEnum options={ options } { ...ps } />
-)
+export const ExtBool = withField({ Control: (ps) => {
+    var { dataXPath, formikField, formikForm } = ps;
+    var { setFieldValue } = formikForm;
+    var { value } = formikField;
+
+    var bag = {
+        value,
+        onChange: (next) => setFieldValue(dataXPath, next)
+    };
+
+    return (
+        <ButtonGroup className='mt-1'>
+            <Yes { ...bag }>Ja</Yes>
+            <No { ...bag }>Nein</No>
+            <Unknown { ...bag }>Unbekannt</Unknown>
+        </ButtonGroup>
+    )
+
+    //<GenericEnum enum={ enumeration } { ...ps } />
+}});
+
+var Yes = (ps) => {
+    var { value, onChange } = ps;
+    return (
+        <YNButton
+            active={ value === 'yes' }
+            variant='primary'
+            onClick={ () => onChange('yes') }
+            { ...ps }
+        />
+    )
+}
+
+var No = (ps) => {
+    var { value, onChange } = ps;
+    return (
+        <YNButton
+            active={ value === 'no' }
+            variant='danger'
+            onClick={ () => onChange('no') }
+            { ...ps }
+        />
+    )
+}
+
+var Unknown = (ps) => {
+    var { value, onChange } = ps;
+    return (
+        <YNButton
+            active={ value === 'unknown' }
+            variant='secondary'
+            onClick={ () => onChange('unknown') }
+            { ...ps }
+        />
+    )
+}
+var YNButton = (ps) => {
+    var { variant, active, ...pass } = ps;
+    return (
+        <Button
+            size='sm'
+            variant={ active ? variant : 'outline-secondary' }
+            style={{ minWidth: '60px' }} { ...pass }
+        />
+    )
+}
