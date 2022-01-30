35bee67c66f6e0d4f2349f92f38479b35dc84c9c
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-01-30 20:37:33 +0100

    working on formik version of location form

---
+++ packages/psydb-ui/src/locations/main-form/component.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Button } from '@mpieva/psydb-ui-layout';
+
+import {
+    DefaultForm,
+    Fields,
+    FormBox,
+} from '@mpieva/psydb-ui-lib';
+
+export const Component = (ps) => {
+    var {
+        title,
+        fieldDefinitions,
+        initialValues,
+        onSubmit,
+
+        related,
+        permissions,
+    } = ps;
+
+    return (
+        <FormBox title={ title }>
+            <DefaultForm
+                initialValues={ initialValues }
+                onSubmit={ onSubmit }
+                useAjvAsync
+            >
+                {(formikProps) => (
+                    <>
+                        { /*console.log(formikProps.values) || ''*/ }
+                        <FormFields
+                            fieldDefinitions={ fieldDefinitions }
+                            related={ related }
+                            permissions={ permissions }
+                        />
+                        <Button type='submit'>Speichern</Button>
+                    </>
+                )}
+            </DefaultForm>
+        </FormBox>
+    );
+}
+
+const FormFields = (ps) => {
+    var { fieldDefinitions, related, permissions } = ps;
+    var customFieldBag = {
+        fieldDefinitions,
+        related,
+        extraTypeProps: {
+            'PhoneWithTypeList': { enableFaxNumbers: true }
+        }
+    }
+    return (
+        <>
+            <Fields.Custom { ...customFieldBag } />
+            <ReservationFields
+                type='away-team'
+                related={ related }
+                permissions={ permission }
+            />
+            <Fields.AccessRightByResearchGroupList
+                label='Zugriff auf diesen Datensatz für'
+                dataXPath='$.systemPermissions.accessRightsByResearchGroup'
+                related={ related }
+                required
+            />
+        </>
+    );
+}
+
+const ReservationFields = (ps) => {
+    var { type, ...pass } = ps;
+    switch (type) {
+        case 'away-team':
+            return <AwayTeamReservationFields { ...pass } />
+        case 'inhouse':
+            return <InhouseReservationFields { ...pass } />
+        case 'no-reservation':
+            return null
+    }
+}
+
+const AwayTeamReservationFields = (ps) => {
+    var { related, permissions } = ps;
+    var prefix = '$.reservationSettings';
+    return (
+        <>
+            <Field.WeekdayBoolObject
+                label='Termine nicht am'
+                dataXPath={`${prefix}.excludeExperimentWeekdays`}
+            />
+        </>
+    );
+}
+
+const InhouseReservationFields = (ps) => {
+    var { related, permissions } = ps;
+    var prefix = '$.reservationSettings';
+    return (
+        <>
+            <Field.WeekdayBoolObject
+                label='Wochentage'
+                dataXPath={`${prefix}.possibleReservationWeekdays`}
+            />
+            <Field.Time
+                label='Reservierbar Von'
+                dataXPath={`${prefix}.possibleReservationTimeInterval.start`}
+            />
+            <Field.Time
+                label='Reservierbar Bis'
+                dataXPath={`${prefix}.possibleReservationTimeInterval.end`}
+            />
+        </>
+    );
+}
---
+++ packages/psydb-ui/src/locations/main-form/create-defaults.js
@@ -0,0 +1,39 @@
+import { createFieldDefaults } from '@mpieva/psydb-ui-lib';
+
+const {
+    Custom,
+    SystemPermissions
+} = createFieldDefaults;
+
+const MINUTE = 60 * 1000;
+const HOUR = 60 * MINUTE;
+
+const ReservationType = (options) => {
+    var { reservationType } = options;
+    switch (reservationType) {
+        case 'away-team':
+            return { excludeExperimentWeekdays: {}}
+        case 'away-team':
+            return {
+                possibleReservationWeekdays: {
+                    mon: true, tue: true, wed: true, thu: true, fri: true,
+                    sat: false, sun: false
+                },
+                possibleReservationTimeInterval: {
+                    start: 8 * HOUR, end: 16 * HOUR
+                },
+                reservationSlotDuration: 15 * MINUTE
+            }
+        case 'no-reservation':
+            return undefined;
+    }
+}
+
+export const createDefaults = (options) => {
+    var { reservationType, fieldDefinitions, permissions } = options;
+    return {
+        custom: Custom({ fieldDefinitions }),
+        reservationSettings: ReservationDefaults({ reservationType }),
+        systemPermissions: SystemPermissions({ permissions }),
+    }
+}
