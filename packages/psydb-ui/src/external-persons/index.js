79749975061c9356f8129bdfe97cc806a9739395
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-01-30 19:29:43 +0100

    updated external person forms

--- packages/psydb-ui/src/external-persons/index.js
+++ packages/psydb-ui/src/external-persons/index.js
@@ -5,8 +5,17 @@ import {
     withRecordTypeView
 } from '@mpieva/psydb-ui-lib/src/generic-views'
 
+import { RecordCreator } from './record-creator';
+import { RecordEditor } from './record-editor';
+
+const ExternalPersonTypeView = withRecordTypeView({
+    RecordCreator,
+    RecordEditor,
+});
+
 const ExternalPersonCollectionView = withCollectionView({
     collection: 'externalPerson',
+    RecordTypeView: ExternalPersonTypeView,
 });
 
 const ExternalPersons = () => {
---
+++ packages/psydb-ui/src/external-persons/record-editor.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import { only } from '@mpieva/psydb-core-utils';
+import { usePermissions, useSendPatch } from '@mpieva/psydb-ui-hooks';
+import { withRecordEditor } from '@mpieva/psydb-ui-lib';
+import MainForm from './main-form';
+
+const EditForm = (ps) => {
+    var {
+        collection,
+        recordType,
+        id,
+        fetched,
+        onSuccessfulUpdate
+    } = ps;
+
+    var {
+        record,
+        fieldDefinitions,
+        related
+    } = fetched;
+
+    var permissions = usePermissions();
+
+    var send = useSendPatch({
+        collection,
+        recordType,
+        record,
+        onSuccessfulUpdate
+    });
+
+    var initialValues = only({
+        from: record.state,
+        paths: [
+            'custom',
+            'systemPermissions',
+        ]
+    });
+
+    return (
+        <>
+            <MainForm.Component
+                title='Externe Person bearbeiten'
+                fieldDefinitions={ fieldDefinitions }
+                initialValues={ initialValues }
+                onSubmit={ send.exec }
+                related={ related }
+                permissions={ permissions }
+            />
+        </>
+    )
+}
+
+export const RecordEditor = withRecordEditor({
+    EditForm,
+    shouldFetchSchema: false,
+});
+
