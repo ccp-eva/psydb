eae36fe70cdcf0803e42982c76f1c7db60190247
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-02-06 18:53:22 +0100

    age/date varants are now selectable

--- packages/psydb-ui-lib/src/formik/extended-search-fields/date-only-server-side-interval.js
+++ packages/psydb-ui-lib/src/formik/extended-search-fields/date-only-server-side-interval.js
@@ -1,41 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import { withField } from '@cdxoo/formik-utils';
+import { Icons } from '@mpieva/psydb-ui-layout';
 import * as CoreFields from '../fields';
 
 export const DateOnlyServerSideInterval = withField({ Control: (ps) => {
-    var { dataXPath } = ps;
+    var { dataXPath, formikForm } = ps;
+    var { setFieldValue } = formikForm;
+    var [ variant, setVariant ] = useState('age');
+
+    var handleSwitchVariant = (key) => {
+        // FIXME: does not properly reset date felds
+        setFieldValue(dataXPath, undefined);
+        setVariant(key);
+    }
+
     return (
         <div className='border p-3'>
-            <header className='border-bottom pb-1 mb-2'>Datumsbereich</header>
-            <div className='d-flex'>
-                <div className='w-50 flex-grow'>
-                    <CoreFields.DateOnlyServerSide
-                        dataXPath={ `${dataXPath}.interval.start` }
-                        label='Von'
-                    />
-                </div>
-                <div className='w-50 flex-grow'>
-                    <CoreFields.DateOnlyServerSide
-                        dataXPath={ `${dataXPath}.interval.end` }
-                        label='Bis'
-                    />
-                </div>
-            </div>
-            <header className='border-bottom pb-1 mb-2'>Altersbereich</header>
+            <Header
+                isActive={ variant === 'age' }
+                onClick={ () => handleSwitchVariant('age') }
+            >
+                Altersfenster
+            </Header>
             <div className='d-flex'>
                 <div className='w-50 flex-grow'>
                     <CoreFields.AgeFrameBoundary
                         dataXPath={ `${dataXPath}.ageFrame.start` }
                         label='Von'
+                        disabled={ variant !== 'age' }
                     />
                 </div>
                 <div className='w-50 flex-grow'>
                     <CoreFields.AgeFrameBoundary
                         dataXPath={ `${dataXPath}.ageFrame.end` }
                         label='Bis'
+                        disabled={ variant !== 'age' }
+                    />
+                </div>
+            </div>
+            <Header
+                isActive={ variant === 'date' }
+                onClick={ () => handleSwitchVariant('date') }
+            >
+                Datumsbereich
+            </Header>
+            <div className='d-flex'>
+                <div className='w-50 flex-grow'>
+                    <CoreFields.DateOnlyServerSide
+                        dataXPath={ `${dataXPath}.interval.start` }
+                        label='Von'
+                        disabled={ variant !== 'date' }
+                    />
+                </div>
+                <div className='w-50 flex-grow'>
+                    <CoreFields.DateOnlyServerSide
+                        dataXPath={ `${dataXPath}.interval.end` }
+                        label='Bis'
+                        disabled={ variant !== 'date' }
                     />
                 </div>
             </div>
         </div>
     )
 }});
+
+const Header = (ps) => {
+    var { isActive, onClick, children } =ps;
+    var className = classnames([
+        'd-flex align-items-center',
+        'border-bottom pb-1 mb-2 text-bold',
+        isActive ? 'text-primary' : 'text-muted'
+    ]);
+    var role = (
+        'button'
+        //isActive ? '' : 'button'
+    );
+    return (
+        <header role={ role } className={ className } onClick={ onClick }>
+            { isActive
+                ? <Icons.CheckSquareFill />
+                : <Icons.Square />
+            }
+            <span className='ml-2'>
+                { children }
+            </span>
+        </header>
+    )
+}
