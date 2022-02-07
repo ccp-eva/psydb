20e1a61c2eb757e96da65d896f642c14c97122ea
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-02-07 10:23:52 +0100

    removed more obsolete code

--- packages/psydb-ui/src/subjects/extended-search/filters.js
+++ packages/psydb-ui/src/subjects/extended-search/filters.js
@@ -13,9 +13,6 @@ export const Filters = (ps) => {
     var { crtSettings, schema } = ps;
     var { fieldDefinitions } = crtSettings;
     
-    var customGdpr = getCustomFields(schema, 'gdpr');
-    var customScientific = getCustomFields(schema, 'scientific');
-
     return (
         <FormBox title='Suchbedingungen'>
             <Fields.Custom
@@ -28,82 +25,9 @@ export const Filters = (ps) => {
                 subChannelKey='scientific'
                 fieldDefinitions={ fieldDefinitions }
             />
-            {/*<CustomFields
-                dataXPath='$.customGdprFilters'
-                fields={ customGdpr }
-            />
-            <CustomFields
-                dataXPath='$.customScientificFilters'
-                fields={ customScientific }
-            />*/}
             <Button type='submit'>
                 Weiter
             </Button>
         </FormBox>
     )
-}
-
-const getCustomFields = (schema, subChannelKey) => (
-    schema.properties[subChannelKey]
-    .properties.state.properties.custom.properties
-);
-
-const CustomFields = (ps) => {
-    var { dataXPath, fields } = ps;
-    return (
-        <>
-            { Object.keys(fields).map(key => (
-                <SearchFieldWrapper
-                    key={ key }
-                    dataXPath={ `${dataXPath}.${key}` }
-                    schema={ fields[key] }
-                />
-            ))}
-        </>
-    );
-}
-
-const SearchFieldWrapper = (ps) => {
-    var { dataXPath, schema } = ps;
-    var { systemType, systemProps, title } = schema;
-
-    var type = getSearchFieldType(systemType);
-    //console.log({ systemType, type, systemProps, schema });
-    
-    if (!type) {
-        return null;
-    }
-
-    var Component = Fields[type];
-    return (
-        <Component
-            label={ title }
-            dataXPath={ `${dataXPath}` }
-            { ...systemProps }
-        />
-    );
-}
-
-const getSearchFieldType = (type) => {
-    switch (type) {
-        case 'SaneString':
-        case 'BiologicalGender':
-        case 'ExtBool':
-            return type;
-
-        case 'PhoneList':
-        case 'EmailList':
-            return 'SaneString';
-
-        case 'HelperSetItemIdList':
-        case 'HelperSetItemId':
-            return 'HelperSetItemIdList';
-
-        case 'ForeignIdList':
-        case 'ForeignId':
-            return 'ForeignIdList';
-
-        default:
-            return undefined;
-    }
 }
