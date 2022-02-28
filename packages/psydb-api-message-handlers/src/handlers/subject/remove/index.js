273e76f5d77133e68e3cef5a46fcdb4a379846d2
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-02-28 04:55:01 +0100

    mplemented checks for subject deletion

--- packages/psydb-api-message-handlers/src/handlers/subject/remove/index.js
+++ packages/psydb-api-message-handlers/src/handlers/subject/remove/index.js
@@ -35,10 +35,82 @@ handler.checkAllowedAndPlausible = async ({
         lastKnownScientificEventId,
     } = message.payload;
 
-    var allReverseRefs = await fetchRecordReverseRefs({
+    var record = await (
+        db.collection('subject')
+        .findOne(
+            { _id: id },
+            { projection: {
+                'scientific.events': false,
+                'gdpr.events': false
+            }}
+        )
+    );
+    if (!record) {
+        throw new ApiError(404);
+    }
+
+    var reverseRefs = await fetchRecordReverseRefs({
         db,
         recordId: id
     });
+
+    if (allReverseRefs.length > 1) {
+        throw new ApiError(409, {
+            apiStatus: 'RecordHasReverseRefs',
+            data: { reverseRefs }
+        });
+    }
+
+    var { scientific: { state }} = record;
+    var { internals: {
+        invitedForExperiments,
+        participatedInStudies
+    }} = state;
+
+    console.log({
+        invitedForExperiments,
+        participatedInStudies
+    });
+
+    if (invitedForExperiments.length > 0) {
+        var now = new Date();
+        var experiments = (
+            db.collection('experiment').find(
+                { 
+                    _id: { $in: invitedForExperiments.map(it => (
+                        it.experimentId
+                    ))},
+                    'state.isPostprocessed': false,
+                    'state.isCanceled': false
+                },
+                {
+                    _id: true,
+                    type: true,
+                    'state.interval': true,
+                }
+            ).toArray()
+        );
+
+        throw new ApiError(409, {
+            apiStatus: 'SubjectHasExperimentInvitations',
+            data: { experiments }
+        });
+    }
+
+    if (participatedInStudies.length > 0) {
+        var participated = participatedInStudies.filter(it => (
+            it.status === 'participated'
+        ));
+        if (participated.length > 0) {
+            throw new ApiError(409, {
+                apiStatus: 'SubjectHasStudyParticipation',
+                data: { participatedInStudies: participated }
+            });
+        }
+    }
+
+    console.log(state);
+    
 }
 
 handler.triggerSystemEvents = async ({
