3de5dc4c566c5823f46ea7975b6ced7b5c9c185c
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-03-11 00:23:10 +0100

    added missing address quick search field

--- packages/psydb-ui-lib/src/formik/quick-search-fields/index.js
+++ packages/psydb-ui-lib/src/formik/quick-search-fields/index.js
@@ -1,3 +1,11 @@
-export * from './sane-string';
 export * from './integer';
 export * from './date-only-server-side';
+import { SaneString } from './sane-string';
+
+const Address = SaneString;
+
+export {
+    SaneString,
+    Address
+}
+
