f77d1561e27ea89a74334c89870658290d992036
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-04-10 21:46:15 +0200

    removed old code

--- packages/psydb-ui/src/studies/index.js
+++ packages/psydb-ui/src/studies/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 import {
-    withCollectionView,
-    withRecordTypeView
-} from '@mpieva/psydb-ui-lib/src/generic-views'
-
-import {
     Route,
     Switch,
     useRouteMatch,
@@ -13,29 +8,17 @@ import {
     useParams
 } from 'react-router-dom';
 
-import {
-    usePermissions,
-    useFetch,
-} from '@mpieva/psydb-ui-hooks';
-
-import {
-    LoadingIndicator,
-    LinkContainer,
-    PermissionDenied,
-    PageWrappers
-} from '@mpieva/psydb-ui-layout';
-
-import {
-    RedirectOrTypeNav,
-    RecordListContainer,
-    FormBox,
-} from '@mpieva/psydb-ui-lib';
-
-import * as enums from '@mpieva/psydb-schema-enums';
-import allSchemaCreators from '@mpieva/psydb-schema-creators';
+import { usePermissions } from '@mpieva/psydb-ui-hooks';
+import { FormBox } from '@mpieva/psydb-ui-lib';
 
 import StudyRecordContainer from './record-container';
 import StudyRecordForm from './record-form';
+
+import {
+    withCollectionView,
+    withRecordTypeView
+} from '@mpieva/psydb-ui-lib/src/generic-views'
+
 
 const StudyRouting = (ps) => {
     var {
@@ -96,87 +79,7 @@ const StudyTypeView = withRecordTypeView({
 
 const Studies = withCollectionView({
     collection: 'study',
-    //RecordTypeView: StudyRecordTypeView,
     RecordTypeView: StudyTypeView
 });
 
 export default Studies;
-
-//const GenericCollectionView = () => {
-//    var collection = 'study';
-//    var title = enums.collections.getLabel(collection) || collection;
-//    var { hasCustomTypes } = allSchemaCreators[collection];
-//
-//    var { path, url } = useRouteMatch();
-//    
-//    var permissions = usePermissions();
-//    var canRead = permissions.hasFlag('canReadStudies');
-//    if (!canRead) {
-//        return (
-//            <PageWrappers.Level1 title={ title } titleLinkUrl={ url }>
-//                <div className='mt-3'>
-//                    <PermissionDenied />
-//                </div>
-//            </PageWrappers.Level1>
-//        )
-//    }
-//
-//    var [ didFetch, fetched ] = useFetch((agent) => (
-//        agent.fetchCollectionCRTs({ collection })
-//    ), []);
-//
-//    if (!didFetch) {
-//        return <LoadingIndicator size='lg' />
-//    }
-//
-//    var collectionRecordTypes = fetched.data;
-//
-//    // TODO: static types
-//    return (
-//        <PageWrappers.Level1 title={ title } titleLinkUrl={ url }>
-//            {(
-//                hasCustomTypes
-//                ? (
-//                    <RoutingForCustomTypes
-//                        path={ path }
-//                        url={ url }
-//                        collection={ collection }
-//                        collectionRecordTypes={ collectionRecordTypes }
-//                    />
-//                )
-//                : (
-//                    <StudyRecordTypeView
-//                        customRecordTypes={ collectionRecordTypes }
-//                        collection={ collection }
-//                    />
-//                )
-//            )}
-//        </PageWrappers.Level1>
-//    );
-//}
-//
-//const RoutingForCustomTypes = ({
-//    path,
-//    url,
-//    collection,
-//    collectionRecordTypes
-//}) => {
-//    return (
-//        <Switch>
-//            <Route exact path={`${path}`}>
-//                <RedirectOrTypeNav
-//                    baseUrl={ url }
-//                    recordTypes={ collectionRecordTypes }
-//                />
-//            </Route>
-//            <Route path={`${path}/:recordType`}>
-//                <StudyRecordTypeView
-//                    customRecordTypes={ collectionRecordTypes }
-//                    collection={ collection }
-//                />
-//            </Route>
-//        </Switch>
-//    )
-//}
-//
-//export default GenericCollectionView;
