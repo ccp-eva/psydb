953a6902fc2023a37e5407852e514189e684fd86
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-04-11 03:03:04 +0200

    updated study some study participation list code

--- packages/psydb-ui/src/studies/participation/index.js
+++ packages/psydb-ui/src/studies/participation/index.js
@@ -1,20 +1,16 @@
-import React, { useEffect, useReducer, useMemo } from 'react';
+import React, { useState } from 'react';
+import { useRouteMatch, useParams } from 'react-router-dom';
+
+import { keyBy } from '@mpieva/psydb-common-lib';
 
 import {
-    Route,
-    Switch,
-    Redirect,
-    useRouteMatch,
-    useHistory,
-    useParams
-} from 'react-router-dom';
-
-import { Button } from 'react-bootstrap';
-
-import keyBy from '@mpieva/psydb-common-lib/src/key-by';
-import agent from '@mpieva/psydb-ui-request-agents';
+    useFetchAll,
+    useRevision,
+    useModalReducer
+} from '@mpieva/psydb-ui-hooks';
 
 import {
+    Button,
     LoadingIndicator,
     TabNav,
 } from '@mpieva/psydb-ui-layout';
@@ -22,93 +18,64 @@ import {
 import ParticipationList from './participation-list';
 import ParticipationCreateModal from './participation-create-modal';
 
+
 const StudyParticipation = ({}) => {
     var { path, url } = useRouteMatch();
     var { id } = useParams();
 
-    var [ state, dispatch ] = useReducer(reducer, {});
+    var revision = useRevision();
+    var modalReducer = useModalReducer();
 
-    var {
-        subjectRecordTypes,
-        dataBySubjectType,
-        selectedSubjectType,
+    var [ selectedSubjectType, setSelectedSubjectType ] = useState();
 
-        showCreateModal,
-
-        listRevision
-    } = state;
-
-    useEffect(() => {
-        agent.readCustomRecordTypeMetadata()
-        .then(response => {
-            dispatch({ type: 'init-subject-record-types', payload: {
-                ...response.data.data
-            }})
-        });
-        
-        agent.fetchParticipatedSubjectsForStudy({
+    var [ didFetch, fetched ] = useFetchAll((agent) => ({
+        subjectRecordTypes: agent.fetchCollectionCRTs({
+            collection: 'subject'
+        }),
+        participation: agent.fetchParticipatedSubjectsForStudy({
             studyId: id
         })
-        .then(response => {
-            dispatch({ type: 'init-subject-data', payload: {
-                ...response.data.data
-            }})
-        })
-    }, [ id, listRevision ])
+    }), [ id, revision.value ]);
 
-    var [
-        handleShowCreateModal,
-        handleHideCreateModal,
-        
-        handleParticipationCreated,
-    ] = useMemo(() => ([
-        () => dispatch({ type: 'show-create-modal' }),
-        () => dispatch({ type: 'hide-create-modal' }),
-
-        () => dispatch({ type: 'increase-list-revision' }),
-    ]));
-
-    if (!dataBySubjectType || !subjectRecordTypes) {
-        return (
-            <LoadingIndicator size='lg' />
-        ) 
+    if (!didFetch) {
+        return <LoadingIndicator size='lg' />
     }
+
+    var { dataBySubjectType } = fetched.participation.data;
+    var subjectTypeKeys = Object.keys(dataBySubjectType);
+    var allSubjectTypes = keyBy({
+        items: fetched.subjectRecordTypes.data,
+        byProp: 'type'
+    });
 
     if (!selectedSubjectType) {
-        selectedSubjectType = Object.keys(dataBySubjectType)[0];
-    }
-    else {
-        // FIXME: what?
-        selectedSubjectType = selectedSubjectType.subjectType;
+        selectedSubjectType = subjectTypeKeys[0];
     }
 
     return (
         <div className='mt-3'>
 
             <ParticipationCreateModal
-                show={ showCreateModal }
-                onHide={ handleHideCreateModal }
-                onSuccessfulCreate={ handleParticipationCreated }
+                { ...modalReducer.passthrough }
+                onSuccessfulCreate={ revision.up }
                 studyId={ id }
                 subjectRecordType={ selectedSubjectType }
             />
-
-            <TabNav
-                activeKey={ selectedSubjectType }
-                items={ Object.keys(dataBySubjectType).map(type => ({
-                    key: type,
-                    label: subjectRecordTypes[type].state.label
-                }))}
-                onItemClick={ (nextKey) => {
-                    dispatch({ type: 'select-subject-type', payload: {
-                        subjectType: nextKey
-                    }})
-                }}
-            />
+            
+            { subjectTypeKeys > 1 && (
+                <TabNav
+                    activeKey={ selectedSubjectType }
+                    items={ subjectTypeKeys.map(type => ({
+                        key: type,
+                        label: allSubjectTypes[type].label
+                    }))}
+                    onItemClick={ (key) => setSelectedSubjectType(key) }
+                />
+            )}
 
             <div className='mt-3'>
 
-                <Button onClick={ handleShowCreateModal }>
+                <Button onClick={ modalReducer.handleShow }>
                     Probanden hinzufügen
                 </Button>
 
@@ -122,49 +89,4 @@ const StudyParticipation = ({}) => {
     )
 }
 
-const reducer = (state, action) => {
-    var { type, payload } = action;
-    switch (type) {
-        case 'init-subject-data':
-            return ({
-                ...state,
-                dataBySubjectType: payload.dataBySubjectType
-            })
-        case 'init-subject-record-types':
-            return ({
-                ...state,
-                subjectRecordTypes: (
-                    keyBy({
-                        items: payload.customRecordTypes.filter(it => (
-                            it.collection === 'subject'
-                        )),
-                        byProp: 'type',
-                    })
-                )
-            })
-        case 'select-subject-type':
-            return ({
-                ...state,
-                selectedSubjectType: payload
-            })
-        
-        case 'show-create-modal':
-            return {
-                ...state,
-                showCreateModal: true,
-            }
-        case 'hide-create-modal':
-            return {
-                ...state,
-                showCreateModal: false,
-            }
-
-        case 'increase-list-revision':
-            return {
-                ...state,
-                listRevision: (state.listRevision || 0) + 1
-            }
-    }
-}
-        
 export default StudyParticipation;
