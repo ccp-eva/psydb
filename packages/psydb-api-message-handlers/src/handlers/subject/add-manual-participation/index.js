d182ee505e5a6ec3c55bf667a45bf45cbeaf4c18
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-04-11 23:59:49 +0200

    add manual participation now sets extra stuff

--- packages/psydb-api-message-handlers/src/handlers/subject/add-manual-participation/index.js
+++ packages/psydb-api-message-handlers/src/handlers/subject/add-manual-participation/index.js
@@ -1,16 +1,13 @@
 'use strict';
 var debug = require('debug')('psydb:api:message-handlers');
 
-var nanoid = require('nanoid').nanoid;
-
-var ApiError = require('@mpieva/psydb-api-lib/src/api-error'),
-    compareIds = require('@mpieva/psydb-api-lib/src/compare-ids');
-
-var SimpleHandler = require('../../../lib/simple-handler'),
-    checkForeignIdsExist = require('../../../lib/check-foreign-ids-exist'),
-    PushMaker = require('../../../lib/push-maker');
+var { nanoid } = require('nanoid');
+var { without } = require('@mpieva/psydb-core-utils');
+var { ApiError, compareIds } = require('@mpieva/psydb-api-lib');
+var { SimpleHandler, checkForeignIdsExist } = require('../../../lib');
 
 var createSchema = require('./schema');
+
 
 var handler = SimpleHandler({
     messageType: 'subject/add-manual-participation',
@@ -31,26 +28,62 @@ handler.checkAllowedAndPlausible = async ({
     var {
         subjectId,
         studyId,
+        locationId,
+
+        experimentOperatorTeamId,
+        experimentOperatorIds,
     } = message.payload;
 
     var study = await db.collection('study').findOne({ _id: studyId });
-
     // TODO: check study permissions
-
     if (!study) {
-        throw new ApiError(404, 'StudyNotFound');
+        throw new ApiError(409, 'StudyNotFound');
     }
 
     var subject = await db.collection('subject').findOne({ _id: subjectId });
-
     // TODO: check subject permissions agains study permissions
     // TODO: check subject record type agains study subject types
-    
     if (!subject) {
-        throw new ApiError(404, 'SubjectNotFound');
+        throw new ApiError(409, 'SubjectNotFound');
+    }
+    
+    var location = await (
+        db.collection('location').findOne({ _id: locationId })
+    );
+    if (!location) {
+        throw new ApiError(409, 'LocationNotFound');
     }
 
+    if (experimentOperatorTeamId) {
+        var opsTeam = await (
+            db.collection('experimentOperatorTeam')
+            .findOne({ _id: experimentOperatorTeamId })
+        );
+        if (!opsTeam) {
+            throw new ApiError(409, 'ExperimentOperatorTeamNotFound');
+        }
+        cache.experimentOperatorTeam = experimentOperatorTeam;
+    }
+    else {
+        var experimentOperators = await (
+            db.collection('personnel')
+            .find({ _id: { $in: experimentOperatorIds } })
+            .toArray()
+        );
+        if (experimentOperators.length !== experimentOperatorIds.length) {
+            throw new ApiError(409, {
+                apiStatus: 'ExperimentOperatorNotFound',
+                data: without(
+                    experimentOperatorIds,
+                    experimentOperators.map(it => it._id)
+                )
+            });
+        }
+    }
+
+    cache.study = study;
     cache.subject = subject;
+    cache.location = location;
 }
 
 handler.triggerSystemEvents = async ({
@@ -64,23 +97,61 @@ handler.triggerSystemEvents = async ({
 }) => {
     var { type: messageType, payload } = message;
     var {
+        labProcedureType,
         studyId,
         subjectId,
+        locationId,
+
         timestamp,
         status,
+        
+        experimentOperatorTeamId,
+        experimentOperatorIds,
     } = payload;
+
+    var { study, subject, location } = cache;
+
+    var participationItem = {
+        _id: nanoid(),
+
+        type: 'manual',
+        realType: labProcedureType,
+
+        studyId,
+        studyType: study.type,
+        locationId,
+        locationType: location.type,
+
+        timestamp,
+        status,
+    }
+
+    if (experimentOperatorTeamId) {
+        var { experimentOperatorTeam } = cache;
+        var { personnelIds, color } = experimentOperatorTeamId.state;
+        
+        participationItem = {
+            ...participationItem,
+            experimentOperatorTeamId,
+            experimentOperatorTeamColor: color,
+            experimentOperatorIds: personnelIds
+        }
+    }
+    else {
+        participationItem = {
+            ...participationItem,
+            experimentOperatorIds,
+        }
+    }
 
     await dispatch({
         collection: 'subject',
         channelId: subjectId,
         subChannelKey: 'scientific',
         payload: { $push: {
-            'scientific.state.internals.participatedInStudies': {
-                type: 'manual',
-                studyId,
-                timestamp,
-                status,
-            },
+            'scientific.state.internals.participatedInStudies': (
+                participationItem
+            ),
         }}
     });
 }
