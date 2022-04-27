4994d445f15bbb898dea367d7df4c90ffb0e4f1b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-04-27 22:25:41 +0200

    posudeo moved form-box to layout

--- packages/psydb-ui-lib/src/form-box.js
+++ packages/psydb-ui-lib/src/form-box.js
@@ -1,26 +1,3 @@
-import React from 'react';
-import classnames from 'classnames';
-// FIXME: move to layout
-const FormBox = ({
-    title,
-    children,
-    extraClassName,
-    titleClassName,
-}) => {
-    var className = classnames([
-        'border p-3 bg-light',
-        extraClassName
-    ])
-    return (
-        <div className={ className }>
-            <h5 className={ titleClassName }>
-                { title }
-            </h5>
-            <hr />
-            { children }
-        </div>
-
-    )
-}
-
+// FIXME: for compat only
+import { FormBox } from '@mpieva/psydb-ui-layout';
 export default FormBox;
