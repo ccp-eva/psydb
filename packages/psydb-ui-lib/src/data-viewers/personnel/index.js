3603b5d9f3869ac985bd5acc8bfccd76dadd6aae
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-04-28 02:57:26 +0200

    intermediate commit

---
+++ packages/psydb-ui-lib/src/data-viewers/personnel/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { createBase, withPair, addComponents } from '../core';
+import {
+    SaneString,
+    FullText,
+    DefaultBool,
+    SystemPermissions,
+} from '../utility-components';
+
+import ExtraDescription from './extra-description';
+
+const labels = {
+    '/sequenceNumber': 'ID Nr.',
+    
+    '/gdpr/state/firstname': 'Vorname',
+    '/gdpr/state/lastname': 'Nachname',
+    '/gdpr/state/emails': 'E-Mail',
+    '/gdpr/state/phones': 'Telefon',
+    '/gdpr/state/description': 'Beschreibung',
+
+    '/scientific/state/researchGroupSettings': 'Forschungsgruppen'
+    '/scientific/state/systemPermissions': 'Zugriff auf diesen Datensatz für'
+}
+
+const [ Personnel, PersonnelContext ] = createBase();
+addComponents(Personnel, PersonnelContext, labels, [
+    {
+        cname: 'SequenceNumber',
+        path: '/sequenceNumber',
+        Component: withPair(SaneString)
+    },
+    { cname: 'Firstname', path: '/gdpr/state/firstname' },
+    { cname: 'Lastname', path: '/gdpr/state/lastname' },
+    
+    {
+        cname: 'Emails',
+        path: '/gdpr/state/emails',
+        Component: withPair(EmailWithPrimaryList)
+    },
+    
+    {
+        cname: 'Phones',
+        path: '/gdpr/state/phones',
+        Component: withPair(PhoneWithTypeList)
+    },
+    
+    {
+        cname: 'Description',
+        path: '/gdpr/state/description',
+        Component: withPair(FullText)
+    },
+    
+    {
+        cname: 'CanLogIn',
+        path: '/scientific/state/canLogIn',
+        Component: withPair(DefaultBool)
+    },
+    {
+        cname: 'HasRootAcccess',
+        path: '/scientific/state/hasRootAccess',
+        Component: withPair(DefaultBool)
+    },
+    {
+        cname: 'ResearchGroupSettings',
+        path: '/scientific/state/researchGroupSettings',
+        Component: withPair(ResearchGroupSettings)
+    },
+
+    
+    {
+        cname: 'SystemPermissions',
+        path: '/state/systemPermissions',
+        Component: withPair(SystemPermissions)
+    },
+]);
+
+export default Personnel;
