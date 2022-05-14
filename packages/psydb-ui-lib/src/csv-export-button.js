78cfa81c7d2916dc69f4b6114ce7e8024427fd29
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-05-14 02:13:08 +0200

    added auto downsload behavior to export button

--- packages/psydb-ui-lib/src/csv-export-button.js
+++ packages/psydb-ui-lib/src/csv-export-button.js
@@ -29,7 +29,28 @@ const CSVExportButton = (ps) => {
     }, { useEffect: false });
 
     var handleExport = () => {
-        fetcher.exec().then(() => {});
+        fetcher.exec().then((response) => {
+            var { data } = response;
+            console.log(data);
+
+            var blob = new Blob(
+                [ data ],
+                {
+                    encoding: "UTF-8",
+                    type: "application/force-download;charset=UTF-8"
+                }
+            );
+
+            var csvURL = window.URL.createObjectURL(blob);
+            var tempLink = document.createElement('a');
+            tempLink.href = csvURL;
+            tempLink.setAttribute('download', `${collection}-export.csv`);
+
+            // firefox needs the link to be appended to the body
+            document.body.appendChild(tempLink);
+            tempLink.click();
+            document.body.removeChild(tempLink);
+        });
     }
 
     return (
