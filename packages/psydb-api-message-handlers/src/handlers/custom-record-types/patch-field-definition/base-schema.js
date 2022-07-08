7c2aa2268bfac68bb5b1733f2ad8f649e0de39bf
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-07-08 05:01:59 +0200

    added missing field-definition patch handler

---
+++ packages/psydb-api-message-handlers/src/handlers/custom-record-types/patch-field-definition/base-schema.js
@@ -0,0 +1,41 @@
+'use strict';
+var {
+    ExactObject,
+    Id,
+    EventId,
+    IdentifierString,
+    SaneString,
+} = require('@mpieva/psydb-schema-fields');
+
+var {
+    Message,
+} = require('@mpieva/psydb-schema-helpers');
+
+var BaseSchema = () => {
+    return Message({
+        type: `custom-record-types/patch-field-definition`,
+        payload: ExactObject({
+            properties: {
+                id: Id(),
+                lastKnownEventId: EventId(),
+                subChannelKey: {
+                    type: 'string',
+                    enum: [ 'scientific', 'gdpr' ]
+                }, 
+                fieldKey: IdentifierString(),
+                props: {
+                    type: 'object',
+                    properties: {
+                        displayName: SaneString({ minLength: 1 })
+                    }
+                },
+            },
+            required: [
+                'id',
+                'props',
+            ]
+        })
+    });
+}
+
+module.exports = BaseSchema;
