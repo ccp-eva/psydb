f19edd94ed1a91980d1c684967ab9233128690b1
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-07-08 02:08:03 +0200

    removed some props that werent actually needed

--- packages/psydb-ui/src/subjects/editor/index.js
+++ packages/psydb-ui/src/subjects/editor/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
 import { useRevision } from '@mpieva/psydb-ui-hooks';
 import { GenericRecordEditorFooter } from '@mpieva/psydb-ui-lib';
 import { RecordEditor } from '@mpieva/psydb-ui-record-views/subjects';
 
 const SubjectEditorContainer = ({
-    collection,
     recordType,
     onSuccessfulUpdate,
 }) => {
-    var { id } = useParams();
     var revision = useRevision();
 
     var editorBag = {
-        id, collection, recordType,
+        recordType,
         revision, onSuccessfulUpdate
     };
     return (
