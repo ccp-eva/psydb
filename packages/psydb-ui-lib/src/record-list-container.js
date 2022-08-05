be315bf683485e63df44db703e3756b6285c0a6a
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-08-05 06:41:41 +0200

    when user filters basic record list the filters are kept for extended search

--- packages/psydb-ui-lib/src/record-list-container.js
+++ packages/psydb-ui-lib/src/record-list-container.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Base64 } from 'js-base64';
 import { useRouteMatch } from 'react-router-dom';
 
 import {
@@ -11,6 +12,62 @@ import { LinkButton } from '@mpieva/psydb-ui-layout';
 
 import CSVSearchExportButton from './csv-search-export-button';
 import RecordList from './record-list';
+
+var createExtendedSearchQuery = (currentQuery) => {
+    if (!currentQuery) {
+        return undefined;
+    }
+
+    var specialFilters = {};
+    var customFilters = {};
+    var customGdprFilters = {};
+    var customScientificFilters = {};
+    for (var key of Object.keys(currentQuery)) {
+        if (!key.startsWith('/')) {
+            continue;
+        }
+
+        var tokens = key.slice(1).split('/');
+        var tail = tokens.pop();
+        var prefix = '/' + tokens.join('/');
+
+        switch (prefix) {
+            case '/state/custom': 
+                customFilters[tail] = currentQuery[key];
+                break;
+            case '/gdpr/state/custom': 
+                customGdprFilters[tail] = currentQuery[key];
+                break;
+            case '/scientific/state/custom': 
+                customScientificFilters[tail] = currentQuery[key];
+                break;
+            default:
+                specialFilters[tail] = currentQuery[key];
+        }
+    }
+
+    var extQueryData = {
+        ...(Object.keys(specialFilters).length && {
+            specialFilters
+        }),
+        ...(Object.keys(customFilters).length && {
+            customFilters
+        }),
+        ...(Object.keys(customGdprFilters).length && {
+            customGdprFilters
+        }),
+        ...(Object.keys(customScientificFilters).length && {
+            customScientificFilters
+        }),
+    }
+    console.log(extQueryData);
+
+    if (!Object.keys(extQueryData).length) {
+        return undefined;
+    }
+
+    return Base64.encode(JSON.stringify(extQueryData));
+}
 
 const RecordListContainer = ({
     target,
@@ -46,7 +103,7 @@ const RecordListContainer = ({
         ? useURLSearchParamsB64()
         : useState({})
     );
-    //console.log(query);
+    console.log(query);
     var { showHidden = false } = query;
     var setShowHidden = (next) => updateQuery({ ...query, showHidden: next });
 
@@ -54,6 +111,12 @@ const RecordListContainer = ({
     var canUseExtendedSearch = permissions.hasFlag('canUseExtendedSearch');
     var canUseCSVExport = permissions.hasFlag('canUseCSVExport');
 
+    var extQuery = createExtendedSearchQuery(query);
+    var extQueryUrl = (
+        extQuery
+        ? `${url}/extended-search/?tab=filters&formData=${extQuery}`
+        : `${url}/extended-search`
+    );
     //var [ showHidden, setShowHidden ] = useState(false);
 
     return (
@@ -66,7 +129,7 @@ const RecordListContainer = ({
                 )}
                 <div>
                     { enableExtendedSearch && canUseExtendedSearch && (
-                        <LinkButton to={`${url}/extended-search`}>
+                        <LinkButton to={ extQueryUrl }>
                             Erweiterte Suche
                         </LinkButton>
                     )}
