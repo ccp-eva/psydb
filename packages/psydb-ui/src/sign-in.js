d84786e52be46e386f7578833efd9368331c9dd8
Jan Schwalbe <jan.schwalbe@martaflex.de>
2022-11-25 08:04:17 +0100

    corporatized login form

--- packages/psydb-ui/src/sign-in.js
+++ packages/psydb-ui/src/sign-in.js
@@ -4,6 +4,8 @@ import { Button } from '@mpieva/psydb-ui-layout';
 import { useWriteRequest } from '@mpieva/psydb-ui-hooks';
 import { DefaultForm, Fields } from '@mpieva/psydb-ui-lib';
 
+import logoTextColor from './mp-logo-farbig-rgb.svg';
+
 const SignInFormBody = (ps) => {
     var { hasError } = ps;
     return (
@@ -54,17 +56,53 @@ const SignIn = ({ onSignedIn }) => {
             style={{
                 minWidth: '500px',
                 position: 'absolute',
-                top: '38%',
+                top: '34%',
                 left: '50%',
                 transform: 'translate(-50%, -50%)'
             }}
         >
-            <h5
+            {/*<h5
                 className='pt-3 pb-3 pl-4 text-white m-0'
                 style={{ background: '#006c66'}}
             >
                 PsyDB Login
-            </h5>
+            </h5>*/}
+            <div 
+                //className='border-top border-left border-right'
+                style={{
+                    borderBottom: '2px solid #006c66'
+                }}
+            >
+                <div style={{ position: 'relative', overflow: 'hidden' }}>
+                    <h5
+                        className='py-2 m-0 border-top text-muted'
+                        style={{
+                            textAlign: 'right',
+                            width: '310px',
+                            position: 'absolute',
+                            right: 0,
+                            bottom: '5px',
+                            paddingLeft: '20px',
+                            paddingRight: '30px',
+                            //background: '#006c66'
+                            //color: '#006c66',
+                            //color: '#555'
+                        }}
+                    >
+                        PsyDB Login
+                    </h5>
+                    <img
+                        style={{
+                            marginLeft: '-20px',
+                            marginRight: '-20px',
+                            marginTop: '-30px',
+                            //marginBottom: '-20px',
+                            marginBottom: '-25px',
+                        }}
+                        src={ logoTextColor } alt=''
+                    />
+                </div>
+            </div>
             <div className='p-4 bg-light border-left border-bottom border-right'>
                 <DefaultForm
                     useAjvAsync
