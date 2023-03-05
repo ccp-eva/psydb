f2fe564d1d733755d0211f073fa12c4d863b2289
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-03-05 23:07:11 +0100

    added general study shorthand list to personnels related participation component

--- packages/psydb-ui/src/personnel/personnel-participation-list.js
+++ packages/psydb-ui/src/personnel/personnel-participation-list.js
@@ -30,37 +30,45 @@ export const PersonnelParticipationList = (ps) => {
     var { records, related } = fetched.data;
 
     return (
-        <Table className='bg-white border'>
-            <thead>
-                <tr>
-                    <th>Zeitpunkt</th>
-                    <th>Studie</th>
-                    <th>Proband:in</th>
-                </tr>
-            </thead>
-            <tbody>
-                { records.map((it, ix) => (
+        <>
+            <div className='bg-white border p-3 mb-3'>
+                <header><b>In diesen Studien:</b></header>
+                { Object.values(related.study).sort((a,b) => (
+                    a.localeCompare(b)
+                )).join(', ') }
+            </div>
+            <Table className='bg-white border'>
+                <thead>
                     <tr>
-                        <td>{ datefns.format(new Date(it.timestamp), 'dd.MM.yyyy HH:mm') }</td>
-                        <td>{ related.study[it.studyId] }</td>
-                        <td>{ related.subject[it.subjectId] }</td>
-
-                        <td className='d-flex justify-content-end'>
-                            { it.type !== 'manual' && it.experimentId && (
-                                <ExperimentIconButton
-                                    to={`/experiments/${it.type}/${it.experimentId}`}
-                                />
-                            )}
-                            <StudyIconButton
-                                to={`/studies/${it.studyType}/${it.studyId}`}
-                            />
-                            <SubjectIconButton
-                                to={`/subjects/${it.subjectType}/${it.subjectId}`}
-                            />
-                        </td>
+                        <th>Zeitpunkt</th>
+                        <th>Studie</th>
+                        <th>Proband:in</th>
                     </tr>
-                ))}
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                    { records.map((it, ix) => (
+                        <tr>
+                            <td>{ datefns.format(new Date(it.timestamp), 'dd.MM.yyyy HH:mm') }</td>
+                            <td>{ related.study[it.studyId] }</td>
+                            <td>{ related.subject[it.subjectId] }</td>
+
+                            <td className='d-flex justify-content-end'>
+                                { it.type !== 'manual' && it.experimentId && (
+                                    <ExperimentIconButton
+                                        to={`/experiments/${it.type}/${it.experimentId}`}
+                                    />
+                                )}
+                                <StudyIconButton
+                                    to={`/studies/${it.studyType}/${it.studyId}`}
+                                />
+                                <SubjectIconButton
+                                    to={`/subjects/${it.subjectType}/${it.subjectId}`}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
     )
 } 
