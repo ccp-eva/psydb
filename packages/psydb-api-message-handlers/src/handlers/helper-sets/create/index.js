ac68a6aad0ae19ab9f4913e5f3bf93e12ddb99ff
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-04-02 11:38:51 +0200

    added missing file

---
+++ packages/psydb-api-message-handlers/src/handlers/helper-sets/create/index.js
@@ -0,0 +1,31 @@
+'use strict';
+var { ApiError } = require('@mpieva/psydb-api-lib');
+var GenericRecordHandler = require('../../../lib/generic-record-handler');
+
+module.exports = GenericRecordHandler({
+    collection: 'helperSet',
+    op: 'create',
+    triggerSystemEvents: async (context) => {
+        await GenericRecordHandler.triggerSystemEvents(context);
+        
+        var {
+            permissions,
+            cache,
+            dispatch,
+        } = context;
+        
+        var { currentChannelId } = cache;
+
+        var intendedResearchGroupIds = permissions.getResearchGroupIds();
+        for (var it of intendedResearchGroupIds) {
+            dispatch({
+                collection: 'researchGroup',
+                channelId: it,
+                payload: { $push: {
+                    'state.helperSetIds': currentChannelId
+                }}
+            })
+        } 
+    },
+});
+
