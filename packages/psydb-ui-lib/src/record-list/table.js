f4da910378c356ac6dfefb5041f0166743fd7c3b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-04-04 14:58:30 +0200

    removed obsolete includes

--- packages/psydb-ui-lib/src/record-list/table.js
+++ packages/psydb-ui-lib/src/record-list/table.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import {
     Table,
-    Alert,
-} from 'react-bootstrap';
-
-import {
     TableHead,
     TableHeadCustomCols,
     TableEmptyFallback
