7414e7bda800fbc442a75355adc368ef347c886e
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-04-17 06:51:23 +0200

    removed obsolete stuff

--- packages/psydb-ui/src/custom-record-types/index.js
+++ packages/psydb-ui/src/custom-record-types/index.js
@@ -1,38 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import agent from '@mpieva/psydb-ui-request-agents';
-
-import {
-    Route,
-    Switch,
-    useRouteMatch,
-    useHistory,
-    useParams
-} from 'react-router-dom';
+import React from 'react';
 
 import {
     withUntypedCollectionView
 } from '@mpieva/psydb-ui-lib/src/generic-views'
 
-import { LinkContainer, LinkButton } from '@mpieva/psydb-ui-layout';
-
 import RecordListContainer from '@mpieva/psydb-ui-lib/src/record-list-container';
+
 import CreateNewType from './create-new-type';
 import EditorContainer from './editor-container';
-
-//import CreateNewType from './create-new-type';
-//import EditType from './edit';
 import RemoveType from './remove';
-
-//const EditButton = (ps) => {
-//    var { linkBaseUrl, record } = ps;
-//    return (
-//        <LinkButton
-//            size='sm'
-//            to={`${linkBaseUrl}/${record._id}/edit`}>
-//            Bearbeiten
-//        </LinkButton>
-//    )
-//}
 
 const RecordCreator = (ps) => {
     var { onSuccessfulUpdate } = ps;
@@ -78,60 +54,5 @@ const CustomRecordTypes = () => {
         <CRTCollectionView />
     )
 }
-
-/*const CustomRecordTypes = () => {
-    var { path, url } = useRouteMatch();
-    var history =  useHistory();
-
-    return (
-        <div>
-            <header>
-                <LinkContainer to={ url }>
-                    <h1 className='border-bottom mb-0' role='button'>
-                        Datensatz-Typen
-                    </h1>
-                </LinkContainer>
-            </header>
-
-            <Switch>
-                <Route exact path={`${path}`}>
-                    <div className='mt-3'>
-                        <RecordListContainer
-                            linkBaseUrl={ url }
-                            collection='customRecordType'
-                            enableNew={ true }
-                            searchOptions={{
-                                enableResearchGroupFilter: false
-                            }}
-                            defaultSort={{
-                                path: 'state.label',
-                                direction: 'asc',
-                            }}
-
-                            CustomActionListComponent={ EditButton }
-                        />
-                    </div>
-                </Route>
-                <Route path={`${path}/new`}>
-                    <div className='mt-3'>
-                        <CreateNewType onCreated={
-                            ({ id }) => history.push(`${path}/${id}/edit`)
-                        } />
-                    </div>
-                </Route>
-                <Route path={`${path}/:id/edit`}>
-                    <EditType
-                        removeUrl={`${url}/:id/remove`}
-                    />
-                </Route>
-                <Route path={`${path}/:id/remove`}>
-                    <RemoveType
-                        successInfoBackLink={`${url}`}
-                    />
-                </Route>
-            </Switch>
-        </div>
-    )
-}*/
 
 export default CustomRecordTypes;
