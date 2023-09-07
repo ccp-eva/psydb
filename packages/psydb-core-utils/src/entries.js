253f60bff92a70c2c04a0cae5e077cdb141e1d90
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-09-07 05:07:07 +0200

    fixed an issue where entries() keys werent numeric when given an array

--- packages/psydb-core-utils/src/entries.js
+++ packages/psydb-core-utils/src/entries.js
@@ -1,9 +1,16 @@
 'use strict';
-var entries = (that) => (
-    Object.keys(that)
-    .map(key => ([
-        key,
-        that[key]
-    ]))
-);
+var entries = (that) => {
+    var out = (
+        Object.keys(that)
+        .map(key => ([
+            key, that[key]
+        ]))
+    );
+
+    if (Array.isArray(that)) {
+        out = out.map(([ key, value ]) => ([ parseInt(key), value ]))
+    }
+
+    return out;
+};
 module.exports = entries;
