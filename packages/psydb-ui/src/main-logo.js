74090b551f88914985e708d6512226ae7088d003
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-09-15 01:39:35 +0200

    made side nav logo switch

--- packages/psydb-ui/src/main-logo.js
+++ packages/psydb-ui/src/main-logo.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useUILanguage } from '@mpieva/psydb-ui-contexts';
 import { Nav, LinkContainer } from '@mpieva/psydb-ui-layout';
 
 //import logoOrb from './ccp-orb.svg';
 //import logoOrgWireframe from './ccp-orb-monochrome-white.svg';
-import logoTextColor from './mp-logo-farbig-rgb.svg';
-import logoTextMono from './mp-logo-graustufen-negativ-rgb.svg';
+//import logoTextMono from './mp-logo-graustufen-negativ-rgb.svg';
+
+import logoDETextColor from './mp-logo-farbig-rgb.svg';
+import logoENTextColor from './mp-logo-en-farbig-rgb.svg';
+
+const logosByLanguage = {
+    'en': logoENTextColor,
+    'de': logoDETextColor,
+}
 
 const Link = ({
     to,
@@ -17,6 +25,7 @@ const Link = ({
 )
 
 const TextColor = () => {
+    var [ language ] = useUILanguage();
     return (
         <h2
             className='text-center pt-3 pb-1 m-0 border-left border-right bg-white'
@@ -31,7 +40,7 @@ const TextColor = () => {
                             marginTop: '-20px',
                             //marginBottom: '-20px',
                         }}
-                        src={ logoTextColor } alt=''
+                        src={ logosByLanguage[language] } alt=''
                     />
                     <div
                         className='border-top pt-2'
@@ -45,36 +54,36 @@ const TextColor = () => {
     );
 }
 
-const TextMono = () => {
-    return (
-        <h2
-            className='text-center pt-3 pb-1 m-0'
-            style={{ background: '#006c66'}}
-        >
-            <Link to='/'>
-                <div style={{ overflow: 'hidden' }}>
-                    <img
-                        style={{
-                            marginLeft: '-20px',
-                            marginRight: '-20px',
-                            marginTop: '-20px',
-                            //marginBottom: '-20px',
-                        }}
-                        src={ logoTextMono } alt=''
-                    />
-                    <div
-                        className='pt-2'
-                        style={{
-                            fontSize: '1.3rem', color: 'white',
-                            borderTop: '1px solid white'
-                        }}
-                    >
-                        <b>PsyDB</b>
-                    </div>
-                </div>
-            </Link>
-        </h2>
-    );
-}
+//const TextMono = () => {
+//    return (
+//        <h2
+//            className='text-center pt-3 pb-1 m-0'
+//            style={{ background: '#006c66'}}
+//        >
+//            <Link to='/'>
+//                <div style={{ overflow: 'hidden' }}>
+//                    <img
+//                        style={{
+//                            marginLeft: '-20px',
+//                            marginRight: '-20px',
+//                            marginTop: '-20px',
+//                            //marginBottom: '-20px',
+//                        }}
+//                        src={ logoTextMono } alt=''
+//                    />
+//                    <div
+//                        className='pt-2'
+//                        style={{
+//                            fontSize: '1.3rem', color: 'white',
+//                            borderTop: '1px solid white'
+//                        }}
+//                    >
+//                        <b>PsyDB</b>
+//                    </div>
+//                </div>
+//            </Link>
+//        </h2>
+//    );
+//}
 
 export default TextColor;
