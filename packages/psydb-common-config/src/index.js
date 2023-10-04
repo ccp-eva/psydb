7c7f37839b6b0eea51842e24f5c2abdbd3b7d1bc
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-10-04 03:41:30 +0200

    swicthed default laguage/locale to en/en-US

--- packages/psydb-common-config/src/index.js
+++ packages/psydb-common-config/src/index.js
@@ -2,7 +2,7 @@ module.exports = {
     enableMigrationMode: false,
     i18n: {
         enableI18NSelect: true,
-        defaultLanguage: 'de',
-        defaultLocaleCode: 'de',
+        defaultLanguage: 'en',
+        defaultLocaleCode: 'en-US',
     }
 }
