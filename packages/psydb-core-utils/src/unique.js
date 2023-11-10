f5ea9ea93d6db5fd2ee8d4e99461b6dd8b90de4e
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-11-10 05:40:38 +0100

    merged changed to unique core util

--- packages/psydb-core-utils/src/unique.js
+++ packages/psydb-core-utils/src/unique.js
@@ -1,6 +1,30 @@
 'use strict';
-var unique = (ary) => (
-    ary.filter((it, index, self) => self.indexOf(it) === index)
-);
+var defaultTransform = it => it;
+
+var unique = (optionsOrAry) => {
+    var ary = undefined;
+    var transform = defaultTransform;
+
+    if (Array.isArray(optionsOrAry)) {
+        ary = optionsOrAry;
+    }
+    else {
+        var {
+            from,
+            // FIXME: this might be better called transformItem
+            // or just transform
+            transformOption = defaultTransform
+        } = optionsOrAry;
+
+        ary = from;
+        transform = transformOption;
+    }
+
+    return (
+        ary.filter((it, index, self) => self.findIndex((other) => {
+            return transform(other) === transform(it)
+        }) === index)
+    )
+};
 
 module.exports = unique;
