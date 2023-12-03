713f1d92443361b21235f78195d1cad616e6591d
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-12-04 00:01:12 +0100

    fix-add-events list endpoint now has related labels

--- packages/psydb-api/src/endpoints/temp_fixesChecker/fixed-add-event-list/index.js
+++ packages/psydb-api/src/endpoints/temp_fixesChecker/fixed-add-event-list/index.js
@@ -4,10 +4,19 @@ var { unescape } = require('@cdxoo/mongodb-escape-keys');
 var {
     validateOrThrow,
     ResponseBody,
+    withRetracedErrors,
+    aggregateToArray,
+    mappifyPointer,
+    fetchRecordLabelsManual,
 } = require('@mpieva/psydb-api-lib');
 
 var Schema = require('./schema');
 
+var mapArgIds = ({ ops, collection }) => (
+    ops
+    .filter(it => it.collection === collection)
+    .map(it => it.args[0]._id)
+)
 
 var fixedAddEventList = async (context, next) => {
     var { db, permissions, request } = context;
@@ -28,25 +37,39 @@ var fixedAddEventList = async (context, next) => {
         })
     );
 
-    var updates = await (
-        db.collection('temp_fixParticipationUpdates').aggregate([
+    var updates = await withRetracedErrors(
+        aggregateToArray({ db, temp_fixParticipationUpdates: [
             { $match: {
                 source: 'fixAddEvents'
             }},
             { $skip: offset },
             { $limit: limit },
-        ])
-    ).toArray();
+        ]})
+    )
 
     for (var u of updates) {
         var { ops } = u;
         for (var o of ops) {
             o.args = o.args.map(a => unescape(a, { traverseArray: true }))
         }
+
+        //u.experimentIds = filterMap({ from: ops, lambda: (it) => ([
+        //    it.collection === 'experiment',
+        //    it.args[0]._id
+        //])})
+       
+        u.experimentIds = mapArgIds({ ops, collection: 'experiment' });
+        u.subjectIds = mapArgIds({ ops, collection: 'subject' });
     }
 
+    var fromItems = mappifyPointer(updates);
+    var related = await fetchRecordLabelsManual(db, {
+        experiment: fromItems('/experimentIds').flat(),
+        subject: fromItems('/subjectIds').flat()
+    });
+
     context.body = ResponseBody({
-        data: { updates, total },
+        data: { updates, total, related },
     });
 }
 
