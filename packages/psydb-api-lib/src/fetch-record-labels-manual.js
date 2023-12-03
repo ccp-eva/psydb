56d3e768281660e66549f4f94d5e093088a379d5
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-12-03 22:07:32 +0100

    fetchRecordLabelsManual() now handles non crt collections garcefully; also some (non-final) stuff for experiment labels

--- packages/psydb-api-lib/src/fetch-record-labels-manual.js
+++ packages/psydb-api-lib/src/fetch-record-labels-manual.js
@@ -1,6 +1,9 @@
 'use strict';
-var { entries } = require('@mpieva/psydb-core-utils');
+var { entries, groupBy } = require('@mpieva/psydb-core-utils');
 var stringifiers = require('@mpieva/psydb-common-lib/src/field-stringifiers');
+var allSchemaCreators = require('@mpieva/psydb-schema-creators');
+
+var aggregateToArray = require('./aggregate-to-array');
 var fetchAllCRTSettings = require('./fetch-all-crt-settings');
 var createRecordLabel = require('./create-record-label');
 var mergeRecordLabelProjections = require('./merge-record-label-projections');
@@ -8,14 +11,96 @@ var mergeRecordLabelProjections = require('./merge-record-label-projections');
 var fetchRecordLabelsManual = async (db, idsForCollection, options = {}) => {
     var { timezone } = options;
 
+    var collections = groupBy({
+        items: Object.keys(idsForCollection),
+        createKey: (c) => (
+            allSchemaCreators[c].hasCustomTypes ? 'withCRT' : 'noCRT'
+        )
+    });
+
+    var sharedBag = { db, idsForCollection, options };
+    
+    var relatedNoCRT = await handleNoCRT({
+        collections: collections.noCRT,
+        ...sharedBag
+    })
+    var relatedWithCRT = await handleWithCRT({
+        collections: collections.withCRT,
+        ...sharedBag
+    });
+
+    var related = {
+        ...relatedNoCRT,
+        ...relatedWithCRT
+    }
+
+    return related;
+}
+
+var handleNoCRT = async (bag) => {
+    var { db, collections, idsForCollection, options } = bag;
+    var { timezone } = options;
+
+    var related = {};
+    for (var collection of collections) {
+        var ids = idsForCollection[collection];
+        
+        var fallback = [
+            undefined,
+            { format: '${#}', tokens: [ '/_id' ] }
+        ];
+        
+        var [ projection, definition ] = {
+            'experiment': [
+                { 
+                    '__type': { $ifNull: [ '$realType', '$type'] },
+                    'state.interval.start': true,
+                },
+                { format: '${#} ${#}', tokens: [
+                    { dataPointer: '/__type' },
+                    {
+                        systemType: 'DateTime',
+                        dataPointer: '/state/interval/start',
+                    },
+                ]}
+            ]
+        }[collection] || fallback;
+
+        var records = await aggregateToArray({ db, [collection]: [
+            { $match: { _id: { $in: ids } }},
+            { $project: {
+                _id: true,
+                ...projection
+            }}
+        ]})
+        
+        related[collection] = {}
+        for (var record of records) {
+            related[collection][record._id] = createRecordLabel({
+                record,
+                definition,
+                timezone
+            })
+        }
+    }
+
+    return related;
+}
+
+var handleWithCRT = async (bag) => {
+    var { db, collections, idsForCollection, options } = bag;
+    var { timezone } = options;
+
     var allCRTSettings = await fetchAllCRTSettings(db, [
-        ...Object.keys(idsForCollection).map(
+        ...collections.map(
             (collection) => ({ collection })
         )
     ], { wrap: true });
 
     var related = {};
-    for (var [ collection, ids ] of entries(idsForCollection)) {
+    for (var collection of collections) {
+        var ids = idsForCollection[collection];
+
         var recordLabelProjection = mergeRecordLabelProjections(
             allCRTSettings[collection], { as: '_labelProjection' }
         );
@@ -23,21 +108,18 @@ var fetchRecordLabelsManual = async (db, idsForCollection, options = {}) => {
         var records = await (
             db.collection(collection)
             .aggregate([
-                { $match: {
-                    _id: { $in: ids }
-                }},
+                { $match: { _id: { $in: ids } }},
                 { $project: {
-                    _id: true,
-                    type: true,
+                    _id: true, type: true,
                     ...recordLabelProjection
                 }}
             ])
             .toArray()
         );
 
-        related[collection] = records.reduce((acc, record) => ({
-            ...acc,
-            [record._id]: createRecordLabel({
+        related[collection] = {}
+        for (var record of records) {
+            related[collection][record._id] = createRecordLabel({
                 record,
                 definition: (
                     allCRTSettings[collection][record.type]
@@ -46,7 +128,7 @@ var fetchRecordLabelsManual = async (db, idsForCollection, options = {}) => {
                 from: '_labelProjection',
                 timezone
             })
-        }), {});
+        }
     }
     return related;
 }
