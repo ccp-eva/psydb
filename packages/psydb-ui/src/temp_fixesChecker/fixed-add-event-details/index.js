809a1063d1730d1926ba8b2dd2f6aa1e9b259bb1
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-12-04 06:37:58 +0100

    added some functions to fix-add-events-details ui; found issue with missing _rohrpostMetadata in inserted/patched records

--- packages/psydb-ui/src/temp_fixesChecker/fixed-add-event-details/index.js
+++ packages/psydb-ui/src/temp_fixesChecker/fixed-add-event-details/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import { useFetch } from '@mpieva/psydb-ui-hooks';
 
@@ -6,6 +6,8 @@ import {
     LoadingIndicator,
     PageWrappers,
     SplitPartitioned,
+    TabNav,
+    Button,
 } from '@mpieva/psydb-ui-layout';
 
 var findId = ({ ops, collection }) => (
@@ -14,6 +16,9 @@ var findId = ({ ops, collection }) => (
 
 const FixedAddEventDetails = () => {
     var { id } = useParams();
+
+    var [ showEventChain, setShowEventChain ] = useState(false);
+    var [ activeKey, setActiveKey ] = useState('experiment')
 
     var [ didFetch, fetched ] = useFetch((agent) => (
         agent.fetchFixedAddEventDetails({ updateId: id })
@@ -31,9 +36,46 @@ const FixedAddEventDetails = () => {
 
     return (
         <PageWrappers.Level1 title='Fixed Add-Events'>
-            <PageWrappers.Level2 title='List'>
+            <PageWrappers.Level2 title='Details'>
+                <div className='mb-3 pb-3 border-bottom d-flex justify-content-between align-items-end'>
+                    <div>
+                        <b>UpdateID:</b> { id }<br />
+
+                        <a 
+                            className='d-block' target='_blank'
+                            href={`/#/experiments/${experimentId}`}
+                        >
+                            Go to Experiment Details
+                        </a>
+                        { subjectId && (
+                            <a 
+                                className='d-block' target='_blank'
+                                href={`/#/subjects/${subjectId}`}
+                            >
+                                Go to Subject Record Details
+                            </a>
+                        )}
+                    </div>
+                    <Button
+                        onClick={() => setShowEventChain(!showEventChain)}
+                    >Show Event Chain</Button>
+                </div>
+                <TabNav
+                    className='mb-3'
+                    activeKey={ activeKey }
+                    items={[
+                        { key: 'experiment', label: 'Experiment' },
+                        ...(subjectId ? [
+                            { key: 'subject', label: 'Subject' }
+                        ] : [])
+                    ]}
+                    onItemClick={ setActiveKey }
+                />
                 { experimentId && (
-                    <SpooledExperiment id={ experimentId } />
+                    <SpooledExperiment
+                        id={ experimentId }
+                        showEventChain={ showEventChain }
+                    />
                 )}
             </PageWrappers.Level2>
         </PageWrappers.Level1>
@@ -41,7 +83,7 @@ const FixedAddEventDetails = () => {
 }
 
 const SpooledExperiment = (ps) => {
-    var { id } = ps;
+    var { id, showEventChain } = ps;
 
     var [ didFetch, fetched ] = useFetch((agent) => (
         agent.fetchSpooledRecord({ collection: 'experiment', id })
@@ -53,14 +95,21 @@ const SpooledExperiment = (ps) => {
 
     var { record, spooled, eventChain } = fetched.data;
 
+    var partitions = [1,1];
+    partitions.unshift(
+        showEventChain ? 1 : 0
+    );
+
     return (
-        <SplitPartitioned partitions={[1,1,1]}>
-            <div>
-                <b>Event Chain</b>
-                <pre className='bg-light p-3 border'>
-                    { JSON.stringify(eventChain, null, 4) }
-                </pre>
-            </div>
+        <SplitPartitioned partitions={ partitions }>
+            { showEventChain && (
+                <div>
+                    <b>Event Chain</b>
+                    <pre className='bg-light p-3 border'>
+                        { JSON.stringify(eventChain, null, 4) }
+                    </pre>
+                </div>
+            )}
             <div>
                 <b>Cached Record</b>
                 <pre className='bg-light p-3 border'>
