be215bc3e9cf2c3409299dc9f5e72eb42b71e77b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2023-12-18 08:33:06 +0100

    fixed issue in nginx setup disabled some lines for now

--- deploy/systemd/install-ubuntu.sh
+++ deploy/systemd/install-ubuntu.sh
@@ -5,65 +5,74 @@ SCRIPT_DIR=$(dirname "$0")
 
 ### mongodb
 ### https://www.mongodb.com/docs/v6.0/tutorial/install-mongodb-on-ubuntu/
-apt install -y gnupg curl
+sudo apt install -y gnupg curl
 
 curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-   gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" \
-    | tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+    | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 
-apt update
-apt install -y mongodb-org
+sudo apt update
+sudo apt install -y mongodb-org
 
-sed -i -e 's/27017/47017/g' /etc/mongod.conf
+sudo sed -i -e 's/27017/47017/g' /etc/mongod.conf
 
-systemctl enable --now mongod
+sudo systemctl enable --now mongod
 
 
 ### nodejs
 ### https://github.com/nodesource/distributions#installation-instructions
-apt install -y ca-certificates curl gnupg
-mkdir -p /etc/apt/keyrings
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key \
-    | gpg -o /etc/apt/keyrings/nodesource.gpg  --dearmor
+    | sudo gpg -o /etc/apt/keyrings/nodesource.gpg  --dearmor
 
 NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" \
-    | tee /etc/apt/sources.list.d/nodesource.list
+    | sudo tee /etc/apt/sources.list.d/nodesource.list
 
-apt update
-apt install -y nodejs
+sudo apt update
+sudo apt install -y nodejs
 
 
 ### nginx
 ### https://nginx.org/en/linux_packages.html#Ubuntu
-apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+sudo apt install curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl -fsSL https://nginx.org/keys/nginx_signing.key \
-    | gpg -o /usr/share/keyrings/nginx-archive-keyring.gpg --dearmor
+    | sudo gpg -o /usr/share/keyrings/nginx-archive-keyring.gpg --dearmor
 
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/ubuntu $(lsb_release -cs) nginx" \
-    | tee /etc/apt/sources.list.d/nginx.list
+    | sudo tee /etc/apt/sources.list.d/nginx.list
 
 echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
-    | tee /etc/apt/preferences.d/99nginx
+    | sudo tee /etc/apt/preferences.d/99nginx
 
-apt update
-apt install -y nginx
+sudo apt update
+sudo apt install -y nginx
 
-cp -v $SCRIPT_DIR/../dist-configs/nginx/default.nossl-conf \
+sudo cp -v $SCRIPT_DIR/../dist-configs/nginx/default.nossl-conf \
     /etc/nginx/conf.d/default.conf
 
-sed -i -e 's/psydb:8080/127.0.0.1:8080/g'
+sudo sed -i -e 's/psydb:8080/127.0.0.1:8080/g' /etc/nginx/conf.d/default.conf
 
 ### psydb
 ###
-groupadd psydb
-mkdir /srv/psydb-deployment
-useradd -M -d /srv/psydb-deployment -s /sbin/nologin psydb -g psydb
-
-cd /srv/psydb-deployment
-cp -a $SCRIPT_DIR/../../ ./psydb-src
-
-cp ./psydb-src/deploy/systemd/psydb.service /usr/lib/systemd/system/
-
+#sudo groupadd psydb
+#sudo mkdir /srv/psydb-deployment
+#sudo useradd -M -d /srv/psydb-deployment -s /sbin/nologin psydb -g psydb
+#
+#cd /srv/psydb-deployment
+#sudo cp -a $SCRIPT_DIR/../../ ./psydb-src
+#
+#sudo cp ./psydb-src/deploy/systemd/psydb.service /usr/lib/systemd/system/
+#
+#cd ./psydb-src
+#rush update
+#cd ./packages/psydb-ui
+#npm run build
+#
+#cd /srv/psydb-deployment
+#
+#sudo systemctl enable --now psydb
+#sudo systemclt enable --now nginx
