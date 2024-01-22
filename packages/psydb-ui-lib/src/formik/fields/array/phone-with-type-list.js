bf8366e735ff067fbc5453d074bfc12ed4cb4e9b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-01-22 19:59:25 +0100

    fixed an issue with PhoneWithTypeList default item value

--- packages/psydb-ui-lib/src/formik/fields/array/phone-with-type-list.js
+++ packages/psydb-ui-lib/src/formik/fields/array/phone-with-type-list.js
@@ -6,17 +6,7 @@ const PhoneWithTypeListInner = withFieldArray({
     FieldComponent: PhoneWithType,
     //ArrayContentWrapper: 'ObjectArrayContentWrapper',
     ArrayItemWrapper: 'ScalarArrayItemWrapper',
-    defaultItemValue: (ps) => {
-        var { formikField } = ps;
-        var { value = [] } = formikField;
-        
-        if (value.length === 0) {
-            return { isPrimary: true };
-        }
-        else {
-            return { isPrimary: false };
-        }
-    }
+    defaultItemValue: (ps) => ({}),
 });
 
 export const PhoneWithTypeList = (ps) => {
