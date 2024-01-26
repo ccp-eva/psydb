8aa1b193e514cb6617603ea3faec4515d757fec9
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-01-26 07:31:48 +0100

    removed obsolete files

--- packages/psydb-ui-lib/src/participation/for-subject/experiment-edit-form/shared/index.js
+++
@@ -1,6 +0,0 @@
-export { default as withSubjectTypeSelect } from './with-subject-type-select';
-export * from './footer';
-
-export * from './subjects-are-tested-together-field.js';
-export { default as SplitExpSubjectFields } from './split-exp-subject-fields';
-export { default as GroupExpSubjectFields } from './group-exp-subject-fields';
