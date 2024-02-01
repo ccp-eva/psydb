a9a6acac95d1c994fc48e8ce2eed6bd3fe8ca4af
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-02-01 16:10:24 +0100

    core-utils/arrify() now has 'removeNullishScalar' option ignore scalar input value of null/undefined and return [] instead of [ undefined ] / [ null ]

--- packages/psydb-core-utils/src/arrify.js
+++ packages/psydb-core-utils/src/arrify.js
@@ -1,6 +1,12 @@
 'use strict';
-var arrify = (a) => (
-    Array.isArray(a) ? a : [ a ]
-);
+var arrify = (a, options = {}) => {
+    var { removeNullishScalar = false } = options;
+
+    if (removeNullishScalar && (a === null || a === undefined)) {
+        return []
+    }
+
+    return Array.isArray(a) ? a : [ a ];
+};
 
 module.exports = arrify;
