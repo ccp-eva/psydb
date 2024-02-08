d3bc7b981792a40e3302ca673a443776169af143
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-02-08 20:21:50 +0100

    moved remote error related stuff into withDefaultResponseBody as the other file wasnt used anymore

--- packages/psydb-koa-event-middleware/src/helpers/with-default-response-body.js
+++ packages/psydb-koa-event-middleware/src/helpers/with-default-response-body.js
@@ -2,11 +2,27 @@
 var { ResponseBody } = require('@mpieva/psydb-api-lib');
 
 var withDefaultResponseBody = async (context, next) => {
+    var { cache } = context;
     if (!context.response.body) {
-        context.response.body = ResponseBody({
-            statusCode: 200,
-            data: context.modifiedChannels
-        });
+        if (cache.remoteErrors) {
+            //context.status = 700;
+            context.body = ResponseBody({
+                // XXX not sure if 700; but needs handling in
+                // 'with-ajv-errors'
+                statusCode: 200,
+                data: context.modifiedChannels,
+                // XXX: not sure about prop name
+                remoteErrors: cache.remoteErrors.map(it => (
+                    it.getInfo()
+                )),
+            });
+        }
+        else {
+            context.response.body = ResponseBody({
+                statusCode: 200,
+                data: context.modifiedChannels
+            });
+        }
     }
 
     await next();
