24c936680bc45c58e390f76b7a67e98e694fc780
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-02-08 20:18:09 +0100

    experiment create form now displays what subjectType/labMethod was auto-selected

--- packages/psydb-ui-lib/src/participation/for-study/experiment-create-form/shared/with-lab-method-select.js
+++ packages/psydb-ui-lib/src/participation/for-study/experiment-create-form/shared/with-lab-method-select.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { unique, only } from '@mpieva/psydb-core-utils';
 import { fixRelated } from '@mpieva/psydb-ui-utils';
 import { useUITranslation } from '@mpieva/psydb-ui-contexts';
-import { useFetch } from '@mpieva/psydb-ui-hooks';
+import { useFetch, usePermissions } from '@mpieva/psydb-ui-hooks';
 import { LoadingIndicator, FormHelpers } from '@mpieva/psydb-ui-layout';
 import * as Controls from '@mpieva/psydb-ui-form-controls';
 import * as enums from '@mpieva/psydb-schema-enums';
@@ -13,6 +13,7 @@ const withLabMethodSelect = (Component) => {
     var WithLabMethodSelect = (ps) => {
         var { studyId, preselectedSubject } = ps;
         
+        var permissions = usePermissions();
         var translate = useUITranslation();
         var [ labMethod, setLabMethod ] = useState();
 
@@ -32,7 +33,11 @@ const withLabMethodSelect = (Component) => {
         var { records, related } = fixRelated(fetched.data);
 
         var [ autoSelected, enabledLabMethods ] = maybeAutoSelect({
-            selectables: unique(records.map(it => it.type))
+            selectables: unique(
+                records
+                .map(it => it.type)
+                .filter(it => permissions.isLabMethodAvailable(it))
+            )
         });
         if (autoSelected) {
             labMethod = autoSelected;
@@ -52,20 +57,19 @@ const withLabMethodSelect = (Component) => {
 
         return (
             <>
-                { !autoSelected && (
-                    <FormHelpers.InlineWrapper
-                        label={ translate('Lab Workflow Type') }
-                    >
-                        <Controls.GenericEnum
-                            value={ labMethod }
-                            onChange={ setLabMethod }
-                            options={ translate.options(only({
-                                from: enums.labMethods.mapping,
-                                paths: enabledLabMethods
-                            }))}
-                        />
-                    </FormHelpers.InlineWrapper>
-                )}
+                <FormHelpers.InlineWrapper
+                    label={ translate('Lab Workflow Type') }
+                >
+                    <Controls.GenericEnum
+                        value={ labMethod }
+                        onChange={ setLabMethod }
+                        options={ translate.options(only({
+                            from: enums.labMethods.mapping,
+                            paths: enabledLabMethods
+                        }))}
+                        readOnly={ !!autoSelected }
+                    />
+                </FormHelpers.InlineWrapper>
                 { labMethod && (
                     <Component { ...componentBag } />
                 )}
