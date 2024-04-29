b6a214235ab416f578eadab6707beea98d3bbfa0
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-04-29 03:36:18 +0200

    fixed an issue with export button introduced by a patch on useFetch() with 'useEffect: false'

--- packages/psydb-ui-hooks/src/use-fetch.js
+++ packages/psydb-ui-hooks/src/use-fetch.js
@@ -66,12 +66,13 @@ const useFetch = (...args) => {
         var exec = () => {
             dispatch({ type: 'set-transmitting' });
             return createPromise(contextAgent).then((response) => {
-                console.log(response);
+                //console.log(response);
                 dispatch({ type: 'init-data', payload: {
                     response: response,
                     data: response.data.data
                 }})
-                return response.data.data
+                // NOTE: csv export requires response.data
+                return response;
             })
         };
         return { exec, isTransmitting: state.isTransmitting };
