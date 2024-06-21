630bde9c4a9702aefda93e6e32074843eea90c7b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-06-21 07:02:14 +0200

    fixed an issue in option list of <BiologicalGender> formik component

--- packages/psydb-ui-lib/src/formik/fields/scalar/biological-gender.js
+++ packages/psydb-ui-lib/src/formik/fields/scalar/biological-gender.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { omit } from '@mpieva/psydb-core-utils';
 import { useUITranslation } from '@mpieva/psydb-ui-contexts';
 import { GenericEnum } from './generic-enum';
 
-const options = {
-    'male': 'Male',
-    'female': 'Female',
-    'other': 'Other',
-    'unknown': 'Unknown',
-}
-
 export const BiologicalGender = (ps) => {
+    var { enableUnknownValue, enableOtherValue } = ps;
     var translate = useUITranslation();
+
+    var options = {
+        'male': 'Male',
+        'female': 'Female',
+        'other': 'Other',
+        'unknown': 'Unknown',
+    }
+
+    var omissions = [];
+    if (!enableUnknownValue) {
+        omissions.push('unknown');
+    }
+    if (!enableOtherValue) {
+        omissions.push('other');
+    }
+    if (omissions.length > 0) {
+        options = omit({ from: options, paths: omissions });
+    }
+
     return (
         <GenericEnum
             options={ translate.options(options) }
