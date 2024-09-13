750b90ab9f44de3447d61340173988643fcd40c3
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-09-13 11:33:32 +0200

    fixed an issue with running period overlap; when study end time wasnt set

--- packages/psydb-api/src/endpoints/statistics/study/match-running-period-overlap-stage.js
+++ packages/psydb-api/src/endpoints/statistics/study/match-running-period-overlap-stage.js
@@ -1,15 +1,37 @@
 'use strict';
+var { SmartArray } = require('@mpieva/psydb-api-lib');
 
 var MatchRunningPeriodOverlapStage = (bag) => {
-    var { interval } = ps;
+    var { interval = {} } = bag;
     var { start, end } = interval;
     var path = 'state.runningPeriod';
 
-    return { $match: { $or: [
-        IntervalIncludes({ path, value: start }),
-        IntervalIncludes({ path, value: end }),
-        IntervalWithinOurs({ path, interval})
-    ]}}
+    if (!start && !end) {
+        return undefined;
+    }
+
+    return { $match: { $or: SmartArray([
+        { $and: SmartArray([
+            ( end && (
+                { [`${path}.start`]: { $lte: end } } 
+            )),
+            { $or: [
+                { [`${path}.end`]: { $type: 10 }},
+                { [`${path}.end`]: { $exists: false }},
+            ]}, 
+        ])},
+        { $or: SmartArray([
+            start && (
+                IntervalIncludes({ path, value: start })
+            ),
+            end && (
+                IntervalIncludes({ path, value: end })
+            ),
+            (start && end) && (
+                IntervalWithinOurs({ path, interval })
+            ),
+        ])}
+    ])}}
 }
 
 var IntervalIncludes = ({ path, value }) => {
