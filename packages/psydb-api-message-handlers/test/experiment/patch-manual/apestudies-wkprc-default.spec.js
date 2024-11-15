63ca6e9c1af0ba883fd1c56b2018685f98f1dbcc
Jan Schwalbe <jan.schwalbe@martaflex.de>
2024-11-15 05:44:27 +0100

    disabled forced debug scope in specs

--- packages/psydb-api-message-handlers/test/experiment/patch-manual/apestudies-wkprc-default.spec.js
+++ packages/psydb-api-message-handlers/test/experiment/patch-manual/apestudies-wkprc-default.spec.js
@@ -1,5 +1,5 @@
 'use strict';
-require('debug').enable('psydb*');
+//require('debug').enable('psydb*');
 var {
     expect, inline, jsonify, ejson, keys, omit, only, pathify, sift,
     ObjectId, RootHandler
