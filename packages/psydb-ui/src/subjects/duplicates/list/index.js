d77f15fe21ff7466cf95eb4304d3b076a8e15d9b
Jan Schwalbe <jan.schwalbe@martaflex.de>
2025-01-31 09:42:52 +0100

    intermediate commit

--- packages/psydb-ui/src/subjects/duplicates/list/index.js
+++ packages/psydb-ui/src/subjects/duplicates/list/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { useI18N } from '@mpieva/psydb-ui-contexts';
-import { useFetch } from '@mpieva/psydb-ui-hooks';
+import { useI18N, useUIConfig } from '@mpieva/psydb-ui-contexts';
+import { useFetch, useSelectionReducer } from '@mpieva/psydb-ui-hooks';
 import { URL } from '@mpieva/psydb-ui-utils';
-import { Table, TableHead, LoadingIndicator } from '@mpieva/psydb-ui-layout';
+import {
+    Table as BSTable,
+    TableHead,
+    LoadingIndicator,
+    Alert,
+} from '@mpieva/psydb-ui-layout';
+
+import * as Controls from '@mpieva/psydb-ui-form-controls';
 import {
     TableHeadCustomCols,
     TableBodyCustomCols
@@ -12,15 +19,43 @@ const DuplicatesList = (ps) => {
     var { recordType } = ps;
     var [{ translate }] = useI18N();
 
+    var selection = useSelectionReducer({
+        defaultSelection: [
+            '/gdpr/state/custom/lastname',
+            '/scientific/state/custom/dateOfBirth',
+        ]
+    });
+
+    var content = (
+        selection.value.length < 1
+        ? (
+            <Alert variant='info'>
+                <i className='text-muted'>
+                    { translate('Please select at least one field.') }
+                </i>
+            </Alert>
+        )
+        : <FetchingTable recordType={ recordType } selection={ selection } />
+    )
+
+    return (
+        <>
+            <FieldSelection selection={ selection } />
+            { content }
+        </>
+    )       
+}
+
+var FetchingTable = (ps) => {
+    var { recordType, selection } = ps;
+    var [{ translate }] = useI18N();
+    
     var [ didFetch, fetched ] = useFetch((agent) => (
         agent.subject.listDuplicates({
             recordType,
-            inspectedPointers: [
-                '/gdpr/state/custom/lastname',
-                '/scientific/state/custom/dateOfBirth',
-            ]
+            inspectedPointers: selection.value
         })
-    ), [ recordType ]);
+    ), [ recordType, selection.value.join() ]);
 
     if (!didFetch) {
         return <LoadingIndicator size='lg' />
@@ -34,21 +69,50 @@ const DuplicatesList = (ps) => {
 
     var sharedBag = { recordType, inspectedFields, related };
     return (
-        <Table>
-            <TableHead showActionColumn={ false }>
-                <TableHeadCustomCols definitions={ inspectedFields } />
-                <th>
-                    { translate('Duplicates') }
-                </th>
-            </TableHead>
-            <tbody>
-                { aggregateItems.map((it, ix) => (
-                    <DuplicateGroup key={ ix } items={ it } { ...sharedBag} />
-                ))}
-            </tbody>
-        </Table>
+        <>
+            <BSTable className='mb-2'>
+                <TableHead showActionColumn={ false }>
+                    <TableHeadCustomCols definitions={ inspectedFields } />
+                    <th>{ translate('Duplicates') }</th>
+                </TableHead>
+                <tbody>
+                    { aggregateItems.map((it, ix) => (
+                        <DuplicateGroup
+                            key={ ix } items={ it } { ...sharedBag}
+                        />
+                    ))}
+                </tbody>
+            </BSTable>
+            
+            { aggregateItems.length < 1 && (
+                <Alert variant='info'>
+                    <i className='text-muted'>
+                        { translate('No duplicates found.') }
+                    </i>
+                </Alert>
+            )}
+        </>
     )
 
+}
+
+var FieldSelection = (ps) => {
+    var { selection } = ps;
+    var { dev_subjectDuplicatesSearchFields } = useUIConfig();
+
+    return (
+        <div className='d-flex flex-wrap bg-light border p-3 gapx-3 mb-3'>
+            { dev_subjectDuplicatesSearchFields.child.map((it, ix) => (
+                <Controls.PlainCheckbox
+                    key={ ix }
+                    id={ it }
+                    label={ it }
+                    value={ selection.value.includes(it) }
+                    onChange={ () => selection.toggle(it)}
+                />
+            ))}
+        </div>
+    )
 }
 
 const DuplicateGroup = (ps) => {
