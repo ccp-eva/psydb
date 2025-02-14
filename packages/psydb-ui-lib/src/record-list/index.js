c4c383536995f3f98efc30d18b28fc833ac99ec6
Jan Schwalbe <jan.schwalbe@martaflex.de>
2025-02-14 13:18:04 +0100

    fuly removed components marked as obsolete

--- packages/psydb-ui-lib/src/record-list/index.js
+++ packages/psydb-ui-lib/src/record-list/index.js
@@ -1,7 +1,4 @@
 export { default as Table } from './table';
-
-//export { default as FieldDataHeadCols } from './field-data-head-cols';
-//export { default as FieldDataBodyCols } from './field-data-body-cols';
-
 import { default as RecordList } from './record-list';
+
 export default RecordList;
