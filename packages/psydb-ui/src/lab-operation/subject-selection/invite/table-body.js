6655102ce84273dd33bb37250b7d61c7b7457c0e
Jan Schwalbe <jan.schwalbe@martaflex.de>
2025-02-14 11:51:45 +0100

    intermediate commit

--- packages/psydb-ui/src/lab-operation/subject-selection/invite/table-body.js
+++ packages/psydb-ui/src/lab-operation/subject-selection/invite/table-body.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
-import { useUITranslation, useUILocale } from '@mpieva/psydb-ui-contexts';
+import { useI18N } from '@mpieva/psydb-ui-contexts';
 import { usePermissions } from '@mpieva/psydb-ui-hooks';
 import {
     Button,
     EditIconButtonInline
 } from '@mpieva/psydb-ui-layout';
+
+import { TableBodyCustomCols } from '@mpieva/psydb-custom-fields-ui';
 
 import {
     formatDateInterval,
@@ -13,7 +14,6 @@ import {
 } from '@mpieva/psydb-ui-lib';
 import calculateAge from '@mpieva/psydb-ui-lib/src/calculate-age';
 
-import { FieldDataBodyCols } from '@mpieva/psydb-ui-lib/src/record-list';
 //import UpcomingExperiments from '../upcoming-experiments';
 
 const TableBody = (ps) => {
@@ -30,7 +30,8 @@ const TableBody = (ps) => {
     } = ps;
 
     var { records, ...subjectMetadata } = subjectData;
-    
+    var { definitions, related } = subjectMetadata;
+
     var permissions = usePermissions();
     var canWriteSubjects = permissions.hasFlag('canWriteSubjects');
 
@@ -48,6 +49,9 @@ const TableBody = (ps) => {
                         subjectMetadata,
                         subjectExperimentMetadata,
                         
+                        related,
+                        definitions,
+
                         canWriteSubjects,
 
                         onInviteSubject,
@@ -60,21 +64,27 @@ const TableBody = (ps) => {
     );
 }
 
-const TableRow = ({
-    inviteType,
-    desiredTestInterval,
+const TableRow = (ps) => {
+    var {
+        inviteType,
+        desiredTestInterval,
 
-    index,
-    subjectType,
-    record,
-    subjectMetadata,
-    subjectExperimentMetadata,
+        index,
+        subjectType,
+        
+        record,
+        related,
+        definitions,
 
-    canWriteSubjects,
+        subjectMetadata,
+        subjectExperimentMetadata,
 
-    onInviteSubject,
-    onViewSubject
-}) => {
+        canWriteSubjects,
+
+        onInviteSubject,
+        onViewSubject
+    } = ps;
+
     var isRed = (
         record._upcomingExperiments.length > 0
     );
@@ -82,28 +92,18 @@ const TableRow = ({
     var isEven = (index % 2) === 0;
     var className = (
         isRed
-        ? (
-            isEven
-            ? 'bg-medium-red'
-            : 'bg-light-red'
-        )
-        : (
-            isEven
-            ? 'bg-light'
-            : ''
-        )
+        ? ( isEven ? 'bg-medium-red' : 'bg-light-red' )
+        : ( isEven ? 'bg-light' : '' )
     );
     return (
         <>
-            <tr
-                key={record._id}
-                className={ className }
-            >
+            <tr className={ className } >
                 <td>{ record._recordLabel }</td>
-                <FieldDataBodyCols { ...({
-                    record,
-                    ...subjectMetadata
-                }) }/>
+                <TableBodyCustomCols
+                    record={ record }
+                    definitions={ definitions }
+                    related={ related }
+                />
                 <td>
                     { calculateAge({
                         base: record._ageFrameField,
@@ -233,7 +233,7 @@ const Participation = (ps) => {
     var { record, subjectMetadata, className } = ps;
     var { participatedInStudies } = record.scientific.state.internals;
 
-    var translate = useUITranslation();
+    var [{ translate }] = useI18N();
 
     var filtered = (
         participatedInStudies
@@ -281,8 +281,7 @@ const UpcomingExperiments = (ps) => {
         className,
     } = ps;
 
-    var translate = useUITranslation();
-    var locale = useUILocale();
+    var [{ translate, locale }] = useI18N();
 
     var upcoming = (
         records.length > 0
@@ -371,8 +370,8 @@ const ActionButtons = (ps) => {
         onInviteSubject,
         onViewSubject
     } = ps;
-    
-    var translate = useUITranslation();
+   
+    var [{ translate }] = useI18N();
 
     return (
         <>
