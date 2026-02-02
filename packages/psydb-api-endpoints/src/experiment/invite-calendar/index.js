32cc6d639324ed8abd281d037c2ddc63d27aadbd
Jan Schwalbe <jan.schwalbe@martaflex.de>
2026-02-02 12:29:13 +0100

    fixed typo

--- packages/psydb-api-endpoints/src/experiment/invite-calendar/index.js
+++ packages/psydb-api-endpoints/src/experiment/invite-calendar/index.js
@@ -155,7 +155,7 @@ var experimentCalendar = async (context, next) => {
                 ...it,
                 _canFollowUp: studiesById[it.state.studyId].state.enableFollowUpExperiments
             })),
-            experimentOperatorTeamRecords: labTeamsRecords,
+            experimentOperatorTeamRecords: labTeamRecords,
             experimentRelated,
             subjectRecordsById,
             subjectRelated: __subjectRelated,
