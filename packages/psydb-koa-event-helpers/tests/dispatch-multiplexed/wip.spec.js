073f1a698788d87758ebc789c65b50c2778f1f5f
Jan Schwalbe <jan.schwalbe@martaflex.de>
2026-04-21 21:35:57 +0200

    intermediate

--- packages/psydb-koa-event-helpers/tests/dispatch-multiplexed/wip.spec.js
+++ packages/psydb-koa-event-helpers/tests/dispatch-multiplexed/wip.spec.js
@@ -5,36 +5,65 @@ var { ObjectId } = require('@mpieva/psydb-mongo-adapter');
 
 var { dispatchMultiplexed } = require('../../run-context-fns');
 
+
 describe('run-context-fns/dispatch-multiplexed', function () {
-    var db, ids, context;
+    var db, ids, context, now;
     beforeEach(async function () {
         //ids = await this.restore([
         //    '2024-09-26__0717'
         //], { gatherIds: true });
 
+        now = new Date();
         db = this.getDbHandle();
-        context = this.createEngineContext();
+        context = this.createEngineContext({ now });
     });
-
+    
+    // NOTE: dispatch multiplexed can not create new channels
     it('does the thing (root-channel)', async function () {
+        var channelIds = [ 10, 20, 30 ];
+
         var deltas = BaselineDeltas();
         deltas.push(await this.aggregateAll([ 'foo', 'rohrpostEvents' ]));
 
+        await db.collection('foo').insertMany([
+            ...channelIds.map(it => ({ _id: it }))
+        ]);
+
         await dispatchMultiplexed(context, [{
-            collection: 'foo', channelIds: [ 10,20,30 ],
-            type: 'CREATE', payload: { $set: { state: { bar: 42 }}}
+            collection: 'foo', channelIds,
+            type: 'CREATE', payload: { $set: { 'state.bar': 42 }}
         }]);
         
         deltas.push(await this.aggregateAll([ 'foo', 'rohrpostEvents' ]));
-        console.ejson(deltas.getCurrent());
-       
-        var sharedFoo = {
-            '_rohrpostMetadata': BaselineDeltas.AnyRohrpostMeta()
-        };
-        deltas.test({ expected: {
-            '/foo/0': { ...sharedFoo, '_id': 10, 'state/bar': 42 },
-            '/foo/1': { ...sharedFoo, '_id': 20, 'state/bar': 42 },
-            '/foo/2': { ...sharedFoo, '_id': 30, 'state/bar': 42 },
-        }, asFlatEJSON: true });
+      
+        var expected = {};
+        for (var [ix, it] of channelIds.entries()) {
+            expected[`/foo/${ix}`] = {
+                '_rohrpostMetadata': {
+                    'updatedAt': now,
+                    'lastKnownSessionId': BaselineDeltas.AnyObjectId(),
+                    'lastKnownEventId': BaselineDeltas.AnyObjectId(),
+                    'eventIds/0': BaselineDeltas.AnyObjectId(),
+                    'unprocessedEventIds': [],
+                },
+                '_id': it,
+                'state/bar': 42,
+            }
+            expected[`/rohrpostEvents/${ix}`] = {
+                '_id': BaselineDeltas.AnyObjectId(),
+                'collectionName': 'foo',
+                'channelId': it,
+                'timestamp': now,
+                'correlationId': 'corr_1010101',
+                'sessionId': BaselineDeltas.AnyObjectId(),
+                'message': {
+                    'type': 'CREATE',
+                    'personnelId': 'staff_1010101',
+                    'payload': { '~1$set': { '~1state~1bar': 42 }}
+                }
+            }
+        }
+
+        deltas.test({ expected, asFlatEJSON: true });
     });
 });
