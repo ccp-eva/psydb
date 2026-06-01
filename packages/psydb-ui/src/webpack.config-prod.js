edce0e0c67c855624729a5364d0bf47145ace4d3
Jan Schwalbe <jan.schwalbe@martaflex.de>
2026-06-02 01:10:39 +0200

    working on how to best support rolling updates

--- packages/psydb-ui/src/webpack.config-prod.js
+++ packages/psydb-ui/src/webpack.config-prod.js
@@ -1,7 +1,21 @@
 'use strict';
+const { DefinePlugin } = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CompressionPlugin = require("compression-webpack-plugin");
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
+// NOTE: using latest commit as UI build version
+// this will prompt the user to reload UI even though it might not
+// have actually changed
+// im not sure how to best approach this; maybe we can have a placeholder
+// js var in index.html and thed sed -i /// with
+// sha256sum dist/*.js src/template.html | sha256sum
+const UI_BUILD_VERSION = (
+    require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString().trim()
+);
 
 const enableReadableErrors = false;
 
@@ -55,6 +69,12 @@ module.exports = {
         })
     },
     plugins: [
+        new DefinePlugin({
+            '__BUNDLE_VERSION__': JSON.stringify(UI_BUILD_VERSION),
+        }),
+        new WebpackManifestPlugin({
+            seed: { version: UI_BUILD_VERSION }
+        }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './template.html'),
             title: 'psydb',
