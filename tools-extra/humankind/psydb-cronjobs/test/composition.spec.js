4acd18dccfec83cfd76ffaf7b331d05c6ed9b287
Jan Schwalbe <jan.schwalbe@martaflex.de>
2026-06-12 03:14:44 +0200

    changed some test code

--- tools-extra/humankind/psydb-cronjobs/test/composition.spec.js
+++ tools-extra/humankind/psydb-cronjobs/test/composition.spec.js
@@ -6,9 +6,10 @@ var Composition = require('../public-online-form/composition');
 describe('public-online-form', function () {
     var db;
     beforeEach(async function () {
-        await this.restore('init-humankind-with-dummy-data');
+        await this.restore('init-humankind-with-dummy-data-2');
         await this.setupInbox({ fixtures: [
-            'regform-mail-08-final',
+            //'regform-mail-08-final',
+            'regform-mail-parent-only-test',
         ]});
 
         this.createKoaApi();
@@ -30,10 +31,11 @@ describe('public-online-form', function () {
             firstname: 'MMMM',
             lastname: 'EEEE'
         });
-        var childOne = await this.getRecord('subject', {
-            firstname: 'Kindvorname',
-            lastname: 'Kindnachname'
-        });
-        console.dir(ejson(childOne), { depth: null });
+        console.dir(ejson(adult), { depth: null });
+        //var childOne = await this.getRecord('subject', {
+        //    firstname: 'Kindvorname',
+        //    lastname: 'Kindnachname'
+        //});
+        //console.dir(ejson(childOne), { depth: null });
     })
 })
