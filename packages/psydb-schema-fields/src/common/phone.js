4338f6ea04661f3fe999f07aaf2beb8712f119f5
Jan Schwalbe <jan.schwalbe@martaflex.de>
2026-09-02 14:26:27 +0200

    updated Phone field data schema

--- packages/psydb-schema-fields/src/common/phone.js
+++ packages/psydb-schema-fields/src/common/phone.js
@@ -1,11 +1,17 @@
-var Phone = ({
-    ...additionalProps
-}) => ({
-    title: 'Telefonnummer',
-    type: 'string',
-    format: 'phone-number', // custom format
-    systemType: 'Phone',
-    ...additionalProps,
-});
+'use strict';
+var Phone = (keywords = {}) => {
+    var { minLength = 0, extraKeywords } = keywords;
+
+    return {
+        systemType: 'Phone',
+        type: 'string',
+        ...((minLength === 0) ? {
+            format: 'phone-number-optional', // custom format
+        } : {
+            format: 'phone-number', // custom format
+        }),
+        ...extraKeywords
+    }
+}
 
 module.exports = Phone;
