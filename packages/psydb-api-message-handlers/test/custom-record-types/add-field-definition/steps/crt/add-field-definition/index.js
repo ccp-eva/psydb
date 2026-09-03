2c279ac3745906c1beda414fe7b2c0432189d422
Jan Schwalbe <jan.schwalbe@martaflex.de>
2026-09-03 17:34:29 +0200

    wip

--- packages/psydb-api-message-handlers/test/custom-record-types/add-field-definition/steps/crt/add-field-definition/index.js
+++ packages/psydb-api-message-handlers/test/custom-record-types/add-field-definition/steps/crt/add-field-definition/index.js
@@ -1,19 +1,13 @@
 'use strict';
-var kebab = require('just-kebab-case');
-var { FieldDefinitionSchemas } = require('@mpieva/psydb-common-lib');
+var build_step = require('./build-add-field-definition-step');
+var variants = {};
 
-var out = {};
-for (var systemType of Object.keys(FieldDefinitionSchemas)) {
-    var fn = undefined;
-    try {
-        fn = require(`./${kebab(systemType)}.step.js`);
-    }
-    catch (e) {
-        fn = () => step(systemType, async function () {
-            throw e;
-        })
-    }
-    out[systemType] = fn;
-}
+variants['SaneString'] = build_step({
+    systemType: 'SaneString',
+    definitionOptions: {
+        'minLength': 0
+    },
+    extraExpectedDefinitionOptions: {}
+});
 
-module.exports = out;
+module.exports = variants;
